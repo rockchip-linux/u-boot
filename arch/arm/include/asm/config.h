e4b4d4e5f2db0651767da8c0d557b74b320c18cf
cjf <cjf@rockchips.com>
2014-03-12 15:32:23 +0800

    not to relocate ramdisk.

--- arch/arm/include/asm/config.h
+++ arch/arm/include/asm/config.h
@@ -8,7 +8,8 @@
 #define _ASM_CONFIG_H_
 
 //#define CONFIG_LMB
-#define CONFIG_SYS_BOOT_RAMDISK_HIGH
+//no need to relocate.
+//#define CONFIG_SYS_BOOT_RAMDISK_HIGH
 
 #ifdef CONFIG_ARM64
 #define CONFIG_PHYS_64BIT
