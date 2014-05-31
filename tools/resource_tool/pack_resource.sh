d43b2df241e8aeca7e9e2ec175304f8c569026e6
cjf <cjf@rockchips.com>
2014-05-31 12:57:11 +0800

    update pack_resource.sh, usage: sudo ./pack_resource.sh <resources dir> <resource.img>

--- tools/resource_tool/pack_resource.sh
+++ tools/resource_tool/pack_resource.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-# usage: . ./pack_resource.sh --pack|--append resources/ resource.img
+function usage()
+{
+	echo "append resources to resource image."
+	echo "usage:"
+	echo "sudo ./pack_resource <resources dir> <resource.img>"
+}
+
+function die()
+{
+	echo "Die:" $1
+	exit -1
+}
+
 function pack_resource ()
 {
 	local TOOL=`realpath "$1"`
@@ -14,7 +26,7 @@ function pack_resource ()
 	fi
 	$TOOL --pack --image=$IMAGE `find $RESOURCES -type f|sed "s#^$RESOURCES/##"` 
 	if [ -d "$RESOURCES" ];then
-		cd -
+		cd - > /dev/null
 		export OLDPWD=$ori_dir
 	fi
 }
@@ -24,40 +36,52 @@ function append_resource ()
 	local TOOL=`realpath "$1"`
 	local RESOURCES=`realpath "$2"`
 	local IMAGE=$3
-	local TMP_DIR=/tmp/resource_out
-	sudo rm -r $TMP_DIR
-	mkdir $TMP_DIR
+	local TMP_DIR=.resource_tmp
+	rm -r $TMP_DIR 2>/dev/null
+	mkdir $TMP_DIR || die "failed to mkdir $TMP_DIR"
 	if [ -f "$IMAGE" ];then
-		sudo $TOOL --unpack --image=$IMAGE $TMP_DIR 1>/dev/null 2>&1
+		echo "Unpacking old image($IMAGE):"
+		$TOOL --unpack --verbose --image=$IMAGE $TMP_DIR 2>&1|grep entry|sed "s/^.*://"|xargs echo
 	fi
 	if [ -d "$RESOURCES" ];then
 		cp -r $RESOURCES/* $TMP_DIR
 	else
 		cp -r $RESOURCES $TMP_DIR
 	fi
+	echo
 	pack_resource $TOOL $TMP_DIR $IMAGE
+	echo
+	echo "Packed resources:"
+	$TOOL --unpack --verbose --image=$IMAGE $TMP_DIR 2>&1|grep entry|sed "s/^.*://"|xargs echo
+	rm -r $TMP_DIR 2>/dev/null
 }
 
-PACK_OPT="--pack"
-APPEND_OPT="--append"
+RESOURCES=$1
+IMAGE=$2
 
-TOOL=`realpath resource_tool`
-if [ ! -f $TOOL ];then
-	echo "tool not found $TOOL !"
-	return;
+if [ `id -u` -gt 0 ];then
+	usage
+	die
 fi
 
-OPT=$1
-RESOURCES=$2
-IMAGE=$3
-
-if [ ! -e "$RESOURCES" ];then
-	echo "resource not found $RESOURCES !"
-	exit
+if [ -z "$RESOURCES" ];then
+	RESOURCES=resources
 fi
 
 if [ -z "$IMAGE" ];then
 	IMAGE=resource.img
+fi
+
+echo "Append $RESOURCES to $IMAGE..."
+echo
+
+if [ ! -e "$RESOURCES" ];then
+	die "resource not found $RESOURCES !"
+fi
+
+TOOL=`realpath resource_tool`
+if [ ! -f $TOOL ];then
+	die "tool not found $TOOL !"
 fi
 
 append_resource $TOOL $RESOURCES $IMAGE
