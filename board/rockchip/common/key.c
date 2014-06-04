df36468496329e052f446c2d3ab69eaa8bdb8de7
cwz <cwz@rock-chips.com>
2014-06-04 18:15:05 +0800

    rkuboot: key fixed some warning.

--- board/rockchip/common/key.c
+++ board/rockchip/common/key.c
@@ -43,9 +43,9 @@ extern void DRVDelayUs(uint32 us);
 gpio_conf       charge_state_gpio;
 gpio_conf       power_hold_gpio;
 
-key_config		key_rockusb;
-key_config		key_recovery;
-key_config		key_fastboot;
+key_config	key_rockusb;
+key_config	key_recovery;
+key_config	key_fastboot;
 key_config      key_power;
 
 
@@ -54,136 +54,138 @@ key_config      key_power;
 */
 int GetPortState(key_config *key)
 {
-    uint32 tt;
-    uint32 hCnt = 0;
-    adc_conf* adc = &key->key.adc; 
-    int_conf* ioint = &key->key.ioint; 
-    if(key->type == KEY_AD)
-    {
-        // TODO: clk没有配置
-    	for(tt = 0; tt < 10; tt++)
-    	{
-    	    // read special gpio port value.
-    	    uint32 value;
-    	    uint32 timeout = 0;
-            write_XDATA32( adc->ctrl, 0);
-    	    DRVDelayUs(1);
-            write_XDATA32( adc->ctrl, 0x0028|(adc->index));
-    		DRVDelayUs(1);
-    		do{
-    		    value = read_XDATA32(adc->ctrl);
-    		    timeout++;
-            }while((value&0x40)==0);
-    		value = read_XDATA32(adc->data);
-            //printf("adc key = %d\n",value);
-    		//DRVDelayUs(1000);
-    		if( value<=adc->keyValueHigh && value>=adc->keyValueLow)
-                hCnt++;
-    	}
-        write_XDATA32( adc->ctrl, 0);
-        return (hCnt>8);
-    } else if (key->type == KEY_INT) {
-	int state;
+	uint32 tt;
+	uint32 hCnt = 0;
+	adc_conf* adc = &key->key.adc;
+	int_conf* ioint = &key->key.ioint;
 
-	state = gpio_get_value(key->key.ioint.gpio);
-	if (ioint->pressed_state == 0) { // active low
-		return !state;
-	} else {
-		return state;
+	if(key->type == KEY_AD)
+	{
+		// TODO: clk没有配置
+		for(tt = 0; tt < 10; tt++)
+		{
+			// read special gpio port value.
+			uint32 value;
+			uint32 timeout = 0;
+
+			write_XDATA32( adc->ctrl, 0);
+			DRVDelayUs(1);
+			write_XDATA32( adc->ctrl, 0x0028|(adc->index));
+			DRVDelayUs(1);
+			do {
+				value = read_XDATA32(adc->ctrl);
+				timeout++;
+			} while((value&0x40) == 0);
+			value = read_XDATA32(adc->data);
+			//printf("adc key = %d\n",value);
+			//DRVDelayUs(1000);
+			if( value<=adc->keyValueHigh && value>=adc->keyValueLow)
+				hCnt++;
+		}
+		write_XDATA32( adc->ctrl, 0);
+		return (hCnt>8);
+	} else if (key->type == KEY_INT) {
+		int state;
+
+		state = gpio_get_value(key->key.ioint.gpio);
+		if (ioint->pressed_state == 0) { // active low
+			return !state;
+		} else {
+			return state;
+		}
 	}
-    }
 }
 
 
 int checkKey(uint32* boot_rockusb, uint32* boot_recovery, uint32* boot_fastboot)
 {
-    int i;
-    int recovery_key = 0;
 	*boot_rockusb = 0;
 	*boot_recovery = 0;
 	*boot_fastboot = 0;
+
 	printf("checkKey\n");
 	if(GetPortState(&key_rockusb))
 	{
-        *boot_rockusb = 1;
-	    //printf("rockusb key is pressed\n");
+		*boot_rockusb = 1;
+		//printf("rockusb key is pressed\n");
 	}
 	if(GetPortState(&key_recovery))
 	{
-        *boot_recovery = 1;
-	    //printf("recovery key is pressed\n");
+		*boot_recovery = 1;
+		//printf("recovery key is pressed\n");
 	}
 	if(GetPortState(&key_fastboot))
 	{
 		*boot_fastboot = 1;
 		//printf("fastboot key is pressed\n");
 	}
+
 	return 0;
 }
 
 void RockusbKeyInit(key_config *key)
 {
-    key->type = KEY_AD;
+	key->type = KEY_AD;
 	key->key.adc.index = 1;	
-    key->key.adc.keyValueLow = 0;
-    key->key.adc.keyValueHigh= 30;
-    key->key.adc.data = SARADC_BASE;
-    key->key.adc.stas = SARADC_BASE+4;
-    key->key.adc.ctrl = SARADC_BASE+8;
+	key->key.adc.keyValueLow = 0;
+	key->key.adc.keyValueHigh= 30;
+	key->key.adc.data = SARADC_BASE;
+	key->key.adc.stas = SARADC_BASE+4;
+	key->key.adc.ctrl = SARADC_BASE+8;
 }
 
 void RecoveryKeyInit(key_config *key)
 {
-    key->type = KEY_AD;
+	key->type = KEY_AD;
 	key->key.adc.index = 1;	
-    key->key.adc.keyValueLow = 0;
-    key->key.adc.keyValueHigh= 30;
-    key->key.adc.data = SARADC_BASE;
-    key->key.adc.stas = SARADC_BASE+4;
-    key->key.adc.ctrl = SARADC_BASE+8;
+	key->key.adc.keyValueLow = 0;
+	key->key.adc.keyValueHigh= 30;
+	key->key.adc.data = SARADC_BASE;
+	key->key.adc.stas = SARADC_BASE+4;
+	key->key.adc.ctrl = SARADC_BASE+8;
 }
 
 
 void FastbootKeyInit(key_config *key)
 {
-    key->type = KEY_AD;
+	key->type = KEY_AD;
 	key->key.adc.index = 1;	
-    key->key.adc.keyValueLow = 170;
-    key->key.adc.keyValueHigh= 180;
-    key->key.adc.data = SARADC_BASE;
-    key->key.adc.stas = SARADC_BASE+4;
-    key->key.adc.ctrl = SARADC_BASE+8;
+	key->key.adc.keyValueLow = 170;
+	key->key.adc.keyValueHigh= 180;
+	key->key.adc.data = SARADC_BASE;
+	key->key.adc.stas = SARADC_BASE+4;
+	key->key.adc.ctrl = SARADC_BASE+8;
 }
 
 
-void PowerKeyInit()
+void PowerKeyInit(void)
 {
-    int i=0;
-    //power_hold_gpio.name
-    key_power.type = KEY_INT;
-    key_power.key.ioint.name = "power_key";
-    key_power.key.ioint.gpio = (GPIO_BANK0 | GPIO_A5);
-    key_power.key.ioint.flags = IRQ_TYPE_EDGE_FALLING;
-    key_power.key.ioint.pressed_state = 0;
-    key_power.key.ioint.press_time = 0;
-}
-
-int power_hold() {
-    return GetPortState(&key_power);
+	//power_hold_gpio.name
+	key_power.type = KEY_INT;
+	key_power.key.ioint.name = "power_key";
+	key_power.key.ioint.gpio = (GPIO_BANK0 | GPIO_A5);
+	key_power.key.ioint.flags = IRQ_TYPE_EDGE_FALLING;
+	key_power.key.ioint.pressed_state = 0;
+	key_power.key.ioint.press_time = 0;
 }
 
 
-void key_init()
+int power_hold(void)
 {
+	return GetPortState(&key_power);
+}
 
-    charge_state_gpio.name = "charge_state";
-    charge_state_gpio.flags = 0;
-    charge_state_gpio.gpio = (GPIO_BANK0 | GPIO_B0);
-    gpio_direction_input(charge_state_gpio.gpio);
 
-    //power_hold_gpio.name
+void key_init(void)
+{
+	charge_state_gpio.name = "charge_state";
+	charge_state_gpio.flags = 0;
+	charge_state_gpio.gpio = (GPIO_BANK0 | GPIO_B0);
+	gpio_direction_input(charge_state_gpio.gpio);
 
-    RockusbKeyInit(&key_rockusb);
+	//power_hold_gpio.name
+
+	RockusbKeyInit(&key_rockusb);
 	FastbootKeyInit(&key_fastboot);
 	RecoveryKeyInit(&key_recovery);
 	PowerKeyInit();
@@ -193,22 +195,23 @@ void key_init()
 return 0: no charger
 return 1: charging
 */
-int is_charging()
+int is_charging(void)
 {
-    if(charge_state_gpio.name != NULL)
-        return (charge_state_gpio.flags == gpio_get_value(charge_state_gpio.gpio));
-    else return 0;
+	if(charge_state_gpio.name != NULL)
+		return (charge_state_gpio.flags == gpio_get_value(charge_state_gpio.gpio));
+
+	return 0;
 }
 
 void powerOn(void)
 {
-    if(power_hold_gpio.name!=NULL)gpio_direction_output(power_hold_gpio.gpio, power_hold_gpio.flags);
+	if(power_hold_gpio.name != NULL)
+		gpio_direction_output(power_hold_gpio.gpio, power_hold_gpio.flags);
 }
 
 void powerOff(void)
 {
-    if(power_hold_gpio.name!=NULL)gpio_direction_output(power_hold_gpio.gpio, !power_hold_gpio.flags);
+	if(power_hold_gpio.name != NULL)
+		gpio_direction_output(power_hold_gpio.gpio, !power_hold_gpio.flags);
 }
-
-
 
