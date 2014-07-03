dae9e5457f166f2d1ba46baec216edaaf1b1f82c
cwz <cwz@rock-chips.com>
2014-07-03 17:52:13 +0800

    rk audi-s: fixed emmc reset con error.

--- drivers/mmc/rkemmc.c
+++ drivers/mmc/rkemmc.c
@@ -110,9 +110,9 @@ static void emmcreset(void)
 	cru_writel(0x00<<3 | 0x01<<(3+16), CRU_SOFTRSTS_CON(8));
 	udelay(200);
 #elif (CONFIG_RKCHIPTYPE == CONFIG_RK3036)
-	cru_writel(0x01<<3 | 0x01<<(3+16), CRU_SOFTRSTS_CON(6));
+	cru_writel(0x01<<3 | 0x01<<(3+16), CRU_SOFTRSTS_CON(5));
 	udelay(100);
-	cru_writel(0x00<<3 | 0x01<<(3+16), CRU_SOFTRSTS_CON(6));
+	cru_writel(0x00<<3 | 0x01<<(3+16), CRU_SOFTRSTS_CON(5));
 	udelay(200);
 #else
 	#error "PLS config platform for emmc reset!"
