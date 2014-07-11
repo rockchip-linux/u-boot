4f4efe66b620431cb44ddb16d5d67e746809b4cc
zyw <zyw@rock-chips.com>
2014-07-11 10:13:21 +0800

    support rk3036 iomux

--- arch/arm/cpu/armv7/rk32xx/iomux-rk3036.c
+++ arch/arm/cpu/armv7/rk32xx/iomux-rk3036.c
@@ -24,18 +24,44 @@
 #include <common.h>
 #include <asm/arch/rkplat.h>
 
-
 static void rk_pwm_iomux_config(int pwm_id)
 {
-
+	switch (pwm_id) {
+		case RK_PWM0_IOMUX:
+			grf_writel((1<<20)|(1<<4), GRF_GPIO0D_IOMUX);     
+			break;
+		case RK_PWM1_IOMUX:
+			grf_writel((3<<16)|(2<<0), GRF_GPIO0A_IOMUX); 
+			break;
+		case RK_PWM2_IOMUX:
+			grf_writel((3<<18)|(2<<2), GRF_GPIO0A_IOMUX); 
+			break;
+		case RK_PWM3_IOMUX:
+			grf_writel((1<<22)|(1<<6), GRF_GPIO0D_IOMUX); 
+			break;
+		default :
+			debug("RK have not this pwm iomux id!\n");
+			break;
+	}
 }
-
 
 static void rk_i2c_iomux_config(int i2c_id)
 {
-
+	switch (i2c_id) {
+		case RK_I2C0_IOMUX: 
+			grf_writel((0xf<<16)|(1<<2)|(1<<0), GRF_GPIO0A_IOMUX); 
+			break;
+		case RK_I2C1_IOMUX:
+			grf_writel((1<<22)|(1<<20)|(1<<6)|(1<<4), GRF_GPIO0A_IOMUX);
+			break;
+		case RK_I2C2_IOMUX:
+			grf_writel((1<<26)|(1<<24)|(1<<10)|(1<<8), GRF_GPIO2C_IOMUX);
+			break;
+		default :
+			debug("RK have not this i2c iomux id!\n");
+			break;		  
+	}
 }
-
 
 static void rk_lcdc_iomux_config(int lcd_id)
 {
@@ -45,17 +71,46 @@ static void rk_lcdc_iomux_config(int lcd_id)
 
 static void rk_spi_iomux_config(int spi_id)
 {
-
+	switch (spi_id) {
+		case RK_SPI0_CS0_IOMUX:
+			grf_writel((3<<28)|(0xf<<24)|(3<<12)|(0xf<<8), GRF_GPIO1D_IOMUX); 
+			break;
+		case RK_SPI0_CS1_IOMUX:
+			grf_writel((3<<30)|(0xf<<24)|(3<<14)|(0xf<<8), GRF_GPIO1D_IOMUX); 
+			break;
+		default :
+			debug("RK have not this spi iomux id!\n");
+			break;
+	}
 }
-
 
 static void rk_uart_iomux_config(int uart_id)
 {
-
+	switch (uart_id) {
+		case RK_UART_BT_IOMUX:
+			grf_writel(1<<20)|(1<<22)|(1<<4)|(1<<6), GRF_GPIO0C_IOMUX); 
+			break;
+		case RK_UART_BB_IOMUX:
+			grf_writel((1<<28)|(3<<30)|(1<<12)|(1<<14), GRF_GPIO2C_IOMUX); 
+			break;
+		case RK_UART_DBG_IOMUX:
+			grf_writel((0xf<<20)|(2<<6)|(2<<4), GRF_GPIO1C_IOMUX); 
+			break;
+		default:
+			debug("RK have not this uart iomux id!\n");
+			break;		 
+	}
 }
-
 
 static void rk_emmc_iomux_config(int emmc_id)
 {
+	switch (emmc_id) {
+		case RK_EMMC_IOMUX:
+			grf_writel((0xFFFF << 16) | 0xAAAA, GRF_GPIO1D_IOMUX); // emmc data0-7
+			grf_writel((3<<18) | (3<<24) | (2<<2) | (2<<8), GRF_GPIO2A_IOMUX); // emmc cmd, emmc rstn out, emmc clkout
+			break;
+		default:
+			debug("RK have not this emmc iomux id!\n");
+			break;
+	}
 }
-
