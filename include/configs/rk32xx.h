b5bb601f241a49c0a69ce9ea1d2038b6dc4d47a1
cwz <cwz@rock-chips.com>
2014-09-17 10:28:52 +0800

    rk32xx.h: some different config between rk3126 and 3128.

--- include/configs/rk32xx.h
+++ include/configs/rk32xx.h
@@ -208,8 +208,17 @@
 	#undef CONFIG_CMD_CHARGE_ANIM
 	#undef CONFIG_CHARGE_DEEP_SLEEP
 	#undef CONFIG_POWER_FG_ADC
-#elif (CONFIG_RKCHIPTYPE == CONFIG_RK3126) || (CONFIG_RKCHIPTYPE == CONFIG_RK3128)
+#elif (CONFIG_RKCHIPTYPE == CONFIG_RK3126)
 	#include "rkplat/rk30plat.h"
+
+	#undef CONFIG_RK_PWM_REMOTE
+#elif (CONFIG_RKCHIPTYPE == CONFIG_RK3128)
+	#include "rkplat/rk30plat.h"
+
+	#undef CONFIG_UBOOT_CHARGE
+	#undef CONFIG_CMD_CHARGE_ANIM
+	#undef CONFIG_CHARGE_DEEP_SLEEP
+	#undef CONFIG_POWER_FG_ADC
 #else
 	#error: "PLS config chip for rk plat!"
 #endif
