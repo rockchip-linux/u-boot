50f6a8992301f25921dba3855007f9d99f6c4a68
cwz <cwz@rock-chips.com>
2014-12-01 14:02:32 +0800

    armv8: cc option add -mgeneral-regs-only, fixed printf doesn't using q0-q5 neon register.

--- arch/arm/cpu/armv8/config.mk
+++ arch/arm/cpu/armv8/config.mk
@@ -6,7 +6,8 @@
 #
 PLATFORM_RELFLAGS += -fno-common -ffixed-x18
 
-PF_CPPFLAGS_ARMV8 := $(call cc-option, -march=armv8-a)
+PF_CPPFLAGS_ARMV8 := $(call cc-option, -march=armv8-a) \
+			$(call cc-option, -mgeneral-regs-only)
 PF_NO_UNALIGNED := $(call cc-option, -mstrict-align)
 PLATFORM_CPPFLAGS += $(PF_CPPFLAGS_ARMV8)
 PLATFORM_CPPFLAGS += $(PF_NO_UNALIGNED)
