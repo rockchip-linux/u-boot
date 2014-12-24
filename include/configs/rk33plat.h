d17a1ec166e65508cad1b5d72f63a02f58a0b7b5
cwz <cwz@rock-chips.com>
2014-12-24 11:02:07 +0800

    maybach: enable merger miniloader.

--- include/configs/rk33plat.h
+++ include/configs/rk33plat.h
@@ -69,7 +69,6 @@
 /* undef some module for rk chip */
 #if defined(CONFIG_RKCHIP_RK3368)
 	#undef CONFIG_SECUREBOOT_CRYPTO
-	#undef CONFIG_MERGER_MINILOADER
 
 	#undef CONFIG_RK_SPI
 	#undef CONFIG_RK_PL330
