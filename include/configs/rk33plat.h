2bb3c1f9bd6e8fe2c20dd83c3dac594dc89dc7e5
cwz <cwz@rock-chips.com>
2015-03-01 19:19:14 +0800

    maybach: rk33plat.h adjust some default config.

--- include/configs/rk33plat.h
+++ include/configs/rk33plat.h
@@ -74,13 +74,11 @@
 #if defined(CONFIG_RKCHIP_RK3368)
 	#undef CONFIG_SECUREBOOT_CRYPTO
 
-	#undef CONFIG_RK_SPI
+	#undef CONFIG_RK_UMS_BOOT_EN
 	#undef CONFIG_RK_PL330
 	#undef CONFIG_RK_DMAC
-	#undef CONFIG_RK_DEVICEINFO
-	#undef CONFIG_RK_PWM_REMOTE
 
-	/*#undef CONFIG_LCD*/
+	#undef CONFIG_LCD
 	#undef CONFIG_PM_SUBSYSTEM
 #endif
 
@@ -99,7 +97,7 @@
 	#undef CONFIG_MERGER_MINILOADER
 	#undef CONFIG_RK_SDCARD_BOOT_EN
 	#undef CONFIG_RK_FLASH_BOOT_EN
-	#undef CONFIG_RK_SPI
+	#undef CONFIG_RK_UMS_BOOT_EN
 	#undef CONFIG_LCD
 	#undef CONFIG_POWER
 	#undef CONFIG_POWER_RK
@@ -150,20 +148,51 @@
 #endif /* CONFIG_CMD_FASTBOOT */
 
 
+#ifdef CONFIG_RK_UMS_BOOT_EN
+/*
+ * USB Host support, default no using
+ * Please first select USB host controller if you want to use UMS Boot
+ * Up to one USB host controller could be selected to enable for booting
+ * from USB Mass Storage device.
+ *
+ * PLS define a host controler from:
+ *	RKUSB_UMS_BOOT_FROM_OTG
+ *	RKUSB_UMS_BOOT_FROM_HOST1
+ *	RKUSB_UMS_BOOT_FROM_HOST2
+ *	RKUSB_UMS_BOOT_FROM_HSIC
+ *
+ * First define the host controller here
+ */
+
+
+/* Check UMS Boot Host define */
+#define RKUSB_UMS_BOOT_CNT (defined(RKUSB_UMS_BOOT_FROM_OTG) + \
+			    defined(RKUSB_UMS_BOOT_FROM_HOST1) + \
+			    defined(RKUSB_UMS_BOOT_FROM_HOST2) + \
+			    defined(RKUSB_UMS_BOOT_FROM_HSIC))
+
+#if (RKUSB_UMS_BOOT_CNT == 0)
+	#error "PLS Select a USB host controller!"
+#elif (RKUSB_UMS_BOOT_CNT > 1)
+	#error "Only one USB host controller can be selected!"
+#else
+	#define CONFIG_CMD_USB
+	#define CONFIG_USB_STORAGE
+	#define CONFIG_PARTITIONS
+#endif
+
+
 /*
  * USB Host support, default no using
  * please first check plat if you want to using usb host
  */
-#if 0
-/* dwc otg */
-#define CONFIG_USB_DWC_HCD
-/* echi */
-#undef CONFIG_USB_EHCI
-#undef CONFIG_USB_EHCI_RK
-
-#define CONFIG_CMD_USB
-#define CONFIG_USB_STORAGE
+#if defined(RKUSB_UMS_BOOT_FROM_HOST1) || defined(RKUSB_UMS_BOOT_FROM_HSIC)
+	#define CONFIG_USB_EHCI
+	#define CONFIG_USB_EHCI_RK
+#elif defined(RKUSB_UMS_BOOT_FROM_HOST2) || defined(RKUSB_UMS_BOOT_FROM_OTG)
+	#define CONFIG_USB_DWC_HCD
 #endif
+#endif /* CONFIG_RK_UMS_BOOT_EN */
 
 
 /* more config for display */
