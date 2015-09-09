56ee7fec635768758733a0a3d871637a646403d0
Alison Wang <b18965@freescale.com>
2015-09-09 10:22:02 +0800

    arm: mmu: Add missing volatile for reading SCTLR register
    Add 'volatile' qualifier to the asm statement in get_cr()
    so that the statement is not optimized out by the compiler.
    
    (http://comments.gmane.org/gmane.linux.linaro.toolchain/5163)
    
    Without the 'volatile', get_cr() returns a wrong value which
    prevents enabling the MMU  and later causes a PCIE VA access
    failure.
    
    Signed-off-by: Alison Wang <alison.wang@freescale.com>

--- arch/arm/include/asm/system.h
+++ arch/arm/include/asm/system.h
@@ -155,7 +155,7 @@ void flush_l3_cache(void);
 static inline unsigned int get_cr(void)
 {
 	unsigned int val;
-	asm("mrc p15, 0, %0, c1, c0, 0	@ get CR" : "=r" (val) : : "cc");
+	asm volatile("mrc p15, 0, %0, c1, c0, 0	@ get CR" : "=r" (val) : : "cc");
 	return val;
 }
 
