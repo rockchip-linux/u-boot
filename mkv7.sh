47517d9039058d68a0250a50c96cce53969baabe
cwz <cwz@rock-chips.com>
2015-12-04 09:22:03 +0800

    rk plat: compile shell rename for easy spelling.
    Change-Id: Iba300eca1e6a328240cb9f83c0f1cd8bd7ff06ff
    Signed-off-by: cwz <cwz@rock-chips.com>

--- mkv7.sh
+++ mkv7.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo    "******************************"
-echo    "*      Make armv7 Uboot      *"
+echo    "*  Make armv8 aarch32 Uboot  *"
 echo    "******************************"
 make --jobs=`sed -n "N;/processor/p" /proc/cpuinfo|wc -l`
 
