5fe61702e0dec4688ef9c0873d001d3322646215
cwz <cwz@rock-chips.com>
2015-12-04 17:59:44 +0800

    rk loader: Adjust parameter code, close some debug info.
    Change-Id: Ic52ded23e581f3e79d832df4ecbed33894156082
    Signed-off-by: cwz <cwz@rock-chips.com>

--- board/rockchip/common/rkloader/parameter.c
+++ board/rockchip/common/rkloader/parameter.c
@@ -13,54 +13,47 @@
 
 
 BootInfo gBootInfo;
-uint32 parameter_lba;
 
-static int find_mtd_part(cmdline_mtd_partition* this_mtd, const char* part_name)
+static int find_mtd_part(cmdline_mtd_partition *this_mtd, const char *part_name)
 {
-	int i=0;
-	for(i=0; i<this_mtd->num_parts; i++)
-	{
-		if( !strcmp(part_name, (const char*)this_mtd->parts[i].name) )
+	int i = 0;
+	for(i = 0; i < this_mtd->num_parts; i++)
+		if(!strcmp(part_name, (const char *)this_mtd->parts[i].name))
 			return i;
-	}
-	
+
 	return -1;
 }
 
-static int get_rdInfo(PBootInfo pboot_info)
+static int get_rdInfo(PBootInfo info)
 {
 	char *token = NULL;
-	char cmdline[MAX_LINE_CHAR];
+	char cmdline[MAX_LINE_CHAR] = "\0";
 	int len = strlen("initrd=");
 
-	strcpy(cmdline, pboot_info->cmd_line);
-	pboot_info->ramdisk_load_addr = 0;
-	pboot_info->ramdisk_offset = 0;
-	pboot_info->ramdisk_size = 0;
-    
+	strcpy(cmdline, info->cmd_line);
+	info->ramdisk_load_addr = 0;
+	info->ramdisk_offset = 0;
+	info->ramdisk_size = 0;
+
 	token = strtok(cmdline, " ");
-	while(token != NULL)
-	{
-		if( !strncmp(token, "initrd=", len) )
-		{
+	while(token != NULL) {
+		if(!strncmp(token, "initrd=", len)) {
 			token += len;
-			pboot_info->ramdisk_load_addr = simple_strtoull(token, &token, 0);
-			pboot_info->ramdisk_size = 0x80000;
-            
-			if(*token == ',')
-			{
+			info->ramdisk_load_addr = simple_strtoull(token, &token, 0);
+			info->ramdisk_size = 0x80000;
+			if(*token == ',') {
 				token++;
-				pboot_info->ramdisk_size = simple_strtoull(token, NULL, 0);
+				info->ramdisk_size = simple_strtoull(token, NULL, 0);
 			}
-            
 			break;
 		}
 		token = strtok(NULL, " ");
 	}
+
 	return 0;
 }
 
-static unsigned long memparse (char *ptr, char **retptr)
+static unsigned long memparse(char *ptr, char **retptr)
 {
 	unsigned long ret = simple_strtoull (ptr, retptr, 0);
 
@@ -78,10 +71,11 @@ static unsigned long memparse (char *ptr, char **retptr)
 	default:
 		break;
 	}
+
 	return ret;
 }
 
-static int get_part(char* parts, disk_partition_t* this_part, int* part_index)
+static int get_part(char* parts, disk_partition_t *this_part, int *part_index)
 {
 	char delim;
 	unsigned int mask_flags;
@@ -89,19 +83,15 @@ static int get_part(char* parts, disk_partition_t* this_part, int* part_index)
 	unsigned int offset = 0;
 	char name[PART_NAME] = "\0";
 
-	if (*parts == '-')
-	{	/* assign all remaining space to this partition */
+	if (*parts == '-') {
+		/* assign all remaining space to this partition */
 		size = SIZE_REMAINING;
 		parts++;
-	}
-	else
-	{
+	} else
 		size = memparse(parts, &parts);
-	}
-	
+
 	/* check for offset */
-	if (*parts == '@')
-	{
+	if (*parts == '@') {
 		parts++;
 		offset = memparse(parts, &parts);
 	}
@@ -111,36 +101,27 @@ static int get_part(char* parts, disk_partition_t* this_part, int* part_index)
 
 	/* now look for name */
 	if (*parts == '(')
-	{
 		delim = ')';
-	}
-	
-	if (delim)
-	{
+
+	if (delim) {
 		char *p;
-		
-		if ((p = strchr(parts+1, delim)) == 0)
-		{
+
+		if ((p = strchr(parts + 1, delim)) == 0) {
 			return 0;
 		}
-		strncpy(name, parts+1, p-(parts+1));
+		strncpy(name, parts + 1, p - (parts + 1));
 		parts = p + 1;
-	}
-	else
-	{ /* Partition_000 */
+	} else
 		sprintf(name, "Partition_%03d", *part_index);
-	}
-	
+
 	/* test for options */
-	if (strncmp(parts, "ro", 2) == 0)
-	{
+	if (strncmp(parts, "ro", 2) == 0) {
 		mask_flags |= MTD_WRITEABLE;
 		parts += 2;
 	}
-	
+
 	/* if lk is found do NOT unlock the MTD partition*/
-	if (strncmp(parts, "lk", 2) == 0)
-	{
+	if (strncmp(parts, "lk", 2) == 0) {
 		mask_flags |= MTD_POWERUP_LOCK;
 		parts += 2;
 	}
@@ -148,116 +129,98 @@ static int get_part(char* parts, disk_partition_t* this_part, int* part_index)
 	this_part->size = size;
 	this_part->start = offset;
 	this_part->blksz = RK_BLK_SIZE;
-	snprintf((char*)this_part->name, sizeof(this_part->name), "%s", name);
-	snprintf((char*)this_part->type, sizeof(this_part->type), "%s", "raw");
+	snprintf((char *)this_part->name, sizeof(this_part->name), "%s", name);
+	snprintf((char *)this_part->type, sizeof(this_part->type), "%s", "raw");
 
-	if( (++(*part_index) < CONFIG_MAX_PARTITIONS) && (*parts == ',') )
-	{
-		get_part(++parts, this_part+1, part_index);
-	}
+	if((++(*part_index) < CONFIG_MAX_PARTITIONS) && (*parts == ','))
+		get_part(++parts, this_part + 1, part_index);
 
 	return 1;
 }
 
-static int mtdpart_parse(const char* string, cmdline_mtd_partition* this_mtd)
+static int mtdpart_parse(const char *string, cmdline_mtd_partition *this_mtd)
 {
 	char *token = NULL;
-	char *s=NULL;
+	char *s = NULL;
 	char *p = NULL;
-	char cmdline[MAX_LINE_CHAR];
+	char cmdline[MAX_LINE_CHAR] = "\0";
 
 	strcpy(cmdline, string);
-
 	token = strtok(cmdline, " ");
-	while(token != NULL)
-	{
-		if( !strncmp(token, "mtdparts=", strlen("mtdparts=")) )
-		{
+	while (token != NULL) {
+		if (!strncmp(token, "mtdparts=", strlen("mtdparts="))) {
 			s = token + strlen("mtdparts=");
 			break;
-			
 		}
 		token = strtok(NULL, " ");
 	}
-	
-	if(s != NULL)
-	{
+
+	if (s != NULL) {
 		if (!(p = strchr(s, ':')))
 			return 0;
 
-		strncpy(this_mtd->mtd_id, s, p-s);
-		s = p+1;
-		get_part(s, this_mtd->parts+this_mtd->num_parts, &(this_mtd->num_parts));
+		strncpy(this_mtd->mtd_id, s, p - s);
+		s = p + 1;
+		get_part(s, this_mtd->parts + this_mtd->num_parts, &(this_mtd->num_parts));
 	}
 
 	return 1;
 }
 
-
-static int parse_cmdline(PBootInfo pboot_info)
+static int parse_cmdline(PBootInfo info)
 {
-// cmy: 解析命令行，获取分区信息
-	pboot_info->cmd_mtd.num_parts = 0;
-	pboot_info->cmd_mtd.mtd_id[0] = '\0';
- 
-	if(mtdpart_parse(pboot_info->cmd_line, &pboot_info->cmd_mtd))
-	{
-		pboot_info->index_misc = find_mtd_part(&pboot_info->cmd_mtd, PARTNAME_MISC);
-		pboot_info->index_kernel = find_mtd_part(&pboot_info->cmd_mtd, 	PARTNAME_KERNEL);
-		pboot_info->index_boot= find_mtd_part(&pboot_info->cmd_mtd, PARTNAME_BOOT);
-		pboot_info->index_recovery = find_mtd_part(&pboot_info->cmd_mtd, PARTNAME_RECOVERY);
-		pboot_info->index_system = find_mtd_part(&pboot_info->cmd_mtd, PARTNAME_SYSTEM);
-		pboot_info->index_backup = find_mtd_part(&pboot_info->cmd_mtd, PARTNAME_BACKUP);
+	info->cmd_mtd.num_parts = 0;
+	info->cmd_mtd.mtd_id[0] = '\0';
 
-		get_rdInfo(pboot_info);
-        
-		if(pboot_info->index_misc < 0 ||
-			pboot_info->index_kernel < 0 ||
-			pboot_info->index_boot < 0 ||
-			pboot_info->index_recovery < 0)
-		{
-			return -1;
-		}
-	}
-	else
-	{
-		//RkPrintf("ERROR: parse cmdline failed!");
+	if(!mtdpart_parse(info->cmd_line, &info->cmd_mtd))
 		return -2;
-	}
+
+	info->index_misc = find_mtd_part(&info->cmd_mtd, PARTNAME_MISC);
+	info->index_kernel = find_mtd_part(&info->cmd_mtd, PARTNAME_KERNEL);
+	info->index_boot = find_mtd_part(&info->cmd_mtd, PARTNAME_BOOT);
+	info->index_recovery = find_mtd_part(&info->cmd_mtd, PARTNAME_RECOVERY);
+	info->index_system = find_mtd_part(&info->cmd_mtd, PARTNAME_SYSTEM);
+	info->index_backup = find_mtd_part(&info->cmd_mtd, PARTNAME_BACKUP);
+
+	get_rdInfo(info);
+
+	if ((info->index_misc < 0) || (info->index_kernel < 0) \
+		|| (info->index_boot < 0) || (info->index_recovery < 0))
+		return -1;
 
 	return 0;
 }
 
 /* -1:error. */
-static int ParseAtoi( const char * line )
+static int ParseAtoi(const char *line)
 {
 	int base = 10;
 	char max = '9';
 	int v = 0;
 
 	EATCHAR(line, ' ');
-	if(*line == 0) return 0;
-    
-	if( line[1] == 'x' || line[1] == 'X' ) {
+	if (*line == 0) return 0;
+
+	if ((line[1] == 'x') || (line[1] == 'X')) {
 		base = 16;
 		max = 'f';      /* F*/
 		line += 2;
 	}
-	if( base == 10 ) {
-		while( *line >= '0' && *line <= max ) {
-			v *= base ;
-			v += *line-'0';
+	if (base == 10) {
+		while ((*line >= '0') && (*line <= max)) {
+			v *= base;
+			v += *line - '0';
 			line++;
 		}
 	} else {
-		while( *line >= '0' && *line <= max ) {
-                        v *= base ;
-                        if( *line >= 'a' )
-                                v += *line-'a'+10;
-                        else if ( *line >= 'A' )
-                                v += *line-'A'+10;
+		while ((*line >= '0') && (*line <= max)) {
+                        v *= base;
+                        if (*line >= 'a')
+                                v += *line - 'a' + 10;
+                        else if (*line >= 'A')
+                                v += *line - 'A' + 10;
                         else
-                                v += *line-'0';
+                                v += *line - '0';
                         line++;
                 }
 	}
@@ -265,215 +228,135 @@ static int ParseAtoi( const char * line )
 	return v;
 }
 
-
-static void ParseLine(PBootInfo pboot_info, char *line)
+static void ParseLine(PBootInfo info, char *line)
 {
-	if( !memcmp(line, "MAGIC:", strlen("MAGIC:")) )
-	    pboot_info->magic_code = ParseAtoi(line+strlen("MAGIC:"));
-	else if( !memcmp(line, "ATAG:", strlen("ATAG:")) )
-	    pboot_info->atag_addr = ParseAtoi(line+strlen("ATAG:"));
-	else if( !memcmp(line, "MACHINE:", strlen("MACHINE:")) )
-	    pboot_info->machine_type = ParseAtoi(line+strlen("MACHINE:"));
-	else if( !memcmp(line, "CHECK_MASK:", strlen("CHECK_MASK:")) )
-	    pboot_info->check_mask = ParseAtoi(line+strlen("CHECK_MASK:"));
-	else if( !memcmp(line, "KERNEL_IMG:", strlen("KERNEL_IMG:")) )
-	    pboot_info->kernel_load_addr = ParseAtoi(line+strlen("KERNEL_IMG:"));
-	else if( !memcmp(line, "BOOT_IMG:", strlen("BOOT_IMG:")) )
-	    pboot_info->boot_offset = ParseAtoi(line+strlen("BOOT_IMG:"));
-	else if( !memcmp(line, "RECOVERY_IMG:", strlen("RECOVERY_IMG:")) )
-	    pboot_info->recovery_offset = ParseAtoi(line+strlen("RECOVERY_IMG:"));
-	else if( !memcmp(line, "MISC_IMG:", strlen("MISC_IMG:")) )
-	    pboot_info->misc_offset = ParseAtoi(line+strlen("MISC_IMG:"));
-#if 0
-	else if( !memcmp(line, "RECOVER_KEY:", strlen("RECOVER_KEY:")) )
-	{//  RECOVER_KEY: 0,4,A,2,0  ==> GPIO4 PA2 低有效
-		line += strlen("RECOVER_KEY:");
-		EATCHAR(line, ' ');
-		setup_key(line, &key_rockusb);
-	}
-	else if( !memcmp(line, "PRODUCT:", strlen("PRODUCT:")))
-	{
-		line += strlen("PRODUCT:");
-		EATCHAR(line, ' ');
-		strncpy(PRODUCT_NAME, line ,sizeof(PRODUCT_NAME));
-	}
-	else if( !memcmp(line, "PWR_HLD:", strlen("PWR_HLD:")) )
-	{//  RECOVER_KEY: 0,4,A,2,0  ==> GPIO4 PA2 低有效
-		line += strlen("PWR_HLD:");
-		EATCHAR(line, ' ');
-		setup_key(line, &pin_powerHold);
-		if(pin_powerHold.key.gpio.valid)
-			powerOn();
-		}
-	else if( !memcmp(line, "COMBINATION_KEY:", strlen("COMBINATION_KEY:")) )
-	{
-		line += strlen("COMBINATION_KEY:");
-		EATCHAR(line, ' ');
-		setup_key(line, &key_combination[KeyCombinationNum]);
-		KeyCombinationNum++;
-	}
-#endif
-	else if( !memcmp(line, "CMDLINE:", strlen("CMDLINE:")) )
-	{
+	if (!memcmp(line, "MAGIC:", strlen("MAGIC:")))
+		info->magic_code = ParseAtoi(line + strlen("MAGIC:"));
+	else if (!memcmp(line, "ATAG:", strlen("ATAG:")))
+		info->atag_addr = ParseAtoi(line + strlen("ATAG:"));
+	else if (!memcmp(line, "MACHINE:", strlen("MACHINE:")))
+		info->machine_type = ParseAtoi(line + strlen("MACHINE:"));
+	else if (!memcmp(line, "CHECK_MASK:", strlen("CHECK_MASK:")))
+		info->check_mask = ParseAtoi(line + strlen("CHECK_MASK:"));
+	else if (!memcmp(line, "KERNEL_IMG:", strlen("KERNEL_IMG:")))
+		info->kernel_load_addr = ParseAtoi(line + strlen("KERNEL_IMG:"));
+	else if (!memcmp(line, "BOOT_IMG:", strlen("BOOT_IMG:")))
+		info->boot_offset = ParseAtoi(line + strlen("BOOT_IMG:"));
+	else if (!memcmp(line, "RECOVERY_IMG:", strlen("RECOVERY_IMG:")))
+		info->recovery_offset = ParseAtoi(line + strlen("RECOVERY_IMG:"));
+	else if (!memcmp(line, "MISC_IMG:", strlen("MISC_IMG:")))
+		info->misc_offset = ParseAtoi(line + strlen("MISC_IMG:"));
+	else if (!memcmp(line, "CMDLINE:", strlen("CMDLINE:"))) {
 		line += strlen("CMDLINE:");
 		EATCHAR(line, ' ');
-		strcpy( pboot_info->cmd_line, line );
-
-		//printf("ORG CMDLINE: %s\n", pboot_info->cmd_line);
-		parse_cmdline(pboot_info);
-	}
-	// cmy: get firmware version
-	else if( !memcmp(line, "FIRMWARE_VER:", strlen("FIRMWARE_VER:")) )
-	{
+		strcpy(info->cmd_line, line);
+		parse_cmdline(info);
+	} else if (!memcmp(line, "FIRMWARE_VER:", strlen("FIRMWARE_VER:"))) {
+		/* get firmware version */
 		line += strlen("FIRMWARE_VER:");
 		EATCHAR(line, ' ');
-		strcpy( pboot_info->fw_version, line );
-		//printf("FIRMWARE_VER: %s\n", pboot_info->fw_version);
-	}
-	//add by cjf, for fdt
-	else if (!memcmp(line, "FDT_NAME:", strlen("FDT_NAME:")) )
-	{
+		strcpy(info->fw_version, line);
+	} else if (!memcmp(line, "FDT_NAME:", strlen("FDT_NAME:"))) {
 		line += strlen("FDT_NAME:");
 		EATCHAR(line, ' ');
-		strcpy( pboot_info->fdt_name, line );
-		//printf("FDT_NAME: %s\n", pboot_info->fdt_name);
-	}
-	else if (!memcmp(line, "USBUART:", strlen("USBUART:"))) {
+		strcpy(info->fdt_name, line);
+	} else if (!memcmp(line, "USBUART:", strlen("USBUART:"))) {
 		line += strlen("USBUART:");
 		EATCHAR(line, ' ');
 		if(!memcmp(line, "enable", strlen("enable"))) {
 			rkplat_uart2UsbEn(1);
 		}
-	}
-	else
-		;//printf("Unknow param: %s!\n", line);
+	} else
+		debug("Unknow param: %s!\n", line);
 }
 
-//
-// param  字符串
-// line   获取到的一行数据存放在该变量中
-// 返回值 偏移的位置
-//
-static char* getline(char* param, int32 len, char *line)
+/*
+ * param  字符串
+ * line   获取到的一行数据存放在该变量中
+ * 返回值 偏移的位置
+ */
+static char *getline(char *param, int32 len, char *line)
 {
-	int i=0;
-	
-	for(i=0; i<len; i++)
-	{
-		if(param[i]=='\n' || param[i]=='\0')
+	int i = 0;
+
+	for (i = 0; i < len; i++) {
+		if ((param[i] == '\n') || (param[i] == '\0'))
 			break;
-		
-		if(param[i] != '\r')
+		if (param[i] != '\r')
 			*(line++) = param[i];
 	}
-	
-	*line='\0';
-	
-	return param+i+1;
+
+	*line = '\0';
+
+	return param + i + 1;
 }
 
 int CheckParam(PLoaderParam pParam)
 {
 	uint32 crc = 0;
-//	PRINT_D("Enter\n");
-	
-	if(pParam->tag != PARM_TAG)
-	{
+
+	if (pParam->tag != PARM_TAG) {
 		printf("W: Invalid Parameter's tag (0x%08X)!\n", (unsigned int)pParam->tag);
 		return -2;
 	}
 
-	if( pParam->length > (MAX_LOADER_PARAM-12) )
-	{
+	if (pParam->length > (MAX_LOADER_PARAM - 12)) {
 		printf("E: Invalid parameter length(%d)!\n", (int)pParam->length);
 		return -3;
 	}
 
-	crc = CRC_32CheckBuffer((unsigned char*)pParam->parameter, pParam->length+4);
-	if(!crc)
-	{
+	crc = CRC_32CheckBuffer((unsigned char *)pParam->parameter, pParam->length + 4);
+	if (!crc) {
 		printf("E: Para CRC failed!\n");
 		return -4;
 	}
 
-//	pParam->crc = crc;
-
-//	PRINT_D("Leave!\n");
-//	RkPrintf("Check ok!\n");
 	return 0;
 }
 
 int32 GetParam(uint32 param_addr, void *buf)
 {
 	PLoaderParam param = (PLoaderParam)buf;
-	int iResult = 0;
-	int i=0;
-	int iRet = 0;
-	int read_sec = MAX_LOADER_PARAM>>9;
+	int read_sec = MAX_LOADER_PARAM >> 9;
+	int i = 0;
+
 	printf("GetParam\n");
 
-	for(i=0; i<PARAMETER_NUM; i++)
-	{
-		if(StorageReadLba( param_addr+i*PARAMETER_OFFSET, buf, read_sec)==0 )//read_sec
-		{
-			iResult = CheckParam(param);
-			if(iResult >= 0)
-			{
-				printf("check parameter success\n");
+	for (i = 0; i < PARAMETER_NUM; i++) {
+		if (StorageReadLba(param_addr + i * PARAMETER_OFFSET, buf, read_sec) == 0) {
+			if (CheckParam(param) == 0) {
+				debug("check parameter success\n");
 				return 0;
-			}
-			else
-			{
+			} else {
 				printf("Invalid parameter\n");
-				iRet = -1;
+				return -1;
 			}
-		}
-		else
-		{
+		} else {
 			printf("read parameter fail\n");
-			iRet = -2;
+			return -2;
 		}
 	}
 
-	return iRet;
+	return -3;
 }
 
-int32 GetBackupParam(void *buf)
+/* 一行最多1024Bytes */
+void ParseParam(PBootInfo info, char *param, uint32 len)
 {
-	int iResult = -1;
-	return iResult;
-}
-
-int32 BackupParam(void *buf)
-{
-	return 0;
-}
-
-void ParseParam(PBootInfo pboot_info, char *param, uint32 len)
-{
-// 一行最多1024Bytes
-	char *prev_param=NULL;
-	char line[MAX_LINE_CHAR];
+	char *prev_param = NULL;
+	char line[MAX_LINE_CHAR] = "\0";
 	int32 remain_len = (int32)len;
-	
-//	RkPrintf("Enter\n");
-//	RkPrintf("--------------- PARAM ---------------\n");
-	while(remain_len>0)
-	{
-		// 获取一行数据(不含回车换行符，且左边不含空格)
+
+	while(remain_len > 0) {
+		/* 获取一行数据(不含回车换行符，且左边不含空格) */
 		prev_param = param;
 		param = getline(param, remain_len, line);
-		remain_len -= (param-prev_param);
+		remain_len -= (param - prev_param);
 
-//		RkPrintf("%s\n", line);
-		// 去除空行及注释行
-		if( line[0] != 0 && line[0] != '#' )
-		{// 该行不是空行，并且不是注释行
-			ParseLine(pboot_info, line);
-		}
+		/* 去除空行及注释行 */
+		if ((line[0] != 0) && (line[0] != '#'))
+			ParseLine(info, line);
 	}
-//	RkPrintf("--------------------------------------\n");
-//	RkPrintf("Leave\n");
 }
 
 void dump_disk_partitions(void)
@@ -481,18 +364,18 @@ void dump_disk_partitions(void)
 	int i;
 
 	printf("dump_disk_partitions:\n");
-	for(i = 0;i < gBootInfo.cmd_mtd.num_parts;i++) {
+	for(i = 0; i < gBootInfo.cmd_mtd.num_parts; i++) {
 		printf("partition(%s): start=0x%08lX, size=0x%08lX, type=%s\n", \
 				gBootInfo.cmd_mtd.parts[i].name, gBootInfo.cmd_mtd.parts[i].start, \
 				gBootInfo.cmd_mtd.parts[i].size, gBootInfo.cmd_mtd.parts[i].type);
 	}
 }
 
-const disk_partition_t* get_disk_partition(const char* name)
+const disk_partition_t* get_disk_partition(const char *name)
 {
 	int part_index = find_mtd_part(&gBootInfo.cmd_mtd, name);
 	if (part_index < 0) {
-		printf("failed to find part:%s\n", name);
+		printf("failed to find part: '%s'\n", name);
 		return NULL;
 	}
 	return &gBootInfo.cmd_mtd.parts[part_index];
@@ -506,19 +389,17 @@ int load_disk_partitions(void)
 	PLoaderParam param = (PLoaderParam)memalign(ARCH_DMA_MINALIGN, MAX_LOADER_PARAM * PARAMETER_NUM);
 
 	if (!GetParam(0, param)) {
-		ParseParam( &gBootInfo, param->parameter, param->length );
+		ParseParam(&gBootInfo, param->parameter, param->length);
 		cmd_mtd = &(gBootInfo.cmd_mtd);
-		for(i = 0;i < cmd_mtd->num_parts;i++) {
+		for(i = 0; i < cmd_mtd->num_parts; i++) {
 			if (i >= CONFIG_MAX_PARTITIONS) {
 				printf("Failed! Too much partition: %d(%d)\n",
 						cmd_mtd->num_parts, CONFIG_MAX_PARTITIONS);
 				goto end;
 			}
-#ifdef DEBUG
-			printf("partition(%s): offset=0x%08lX, size=0x%08lX\n", \
+			debug("partition(%s): offset=0x%08lX, size=0x%08lX\n", \
 					cmd_mtd->parts[i].name, cmd_mtd->parts[i].start, \
 					cmd_mtd->parts[i].size);
-#endif
 		}
 		ret = 0;
 	}
@@ -528,4 +409,3 @@ end:
 
 	return ret;
 }
-
--- board/rockchip/common/rkloader/parameter.h
+++ board/rockchip/common/rkloader/parameter.h
@@ -7,31 +7,31 @@
 #ifndef _PARAMETER_H_
 #define _PARAMETER_H_
 
-#define PARAMETER_NUM			8			// parameter文件的备份个数
-#define PARAMETER_OFFSET		1024			// 每个parameter的偏移量
+#define PARAMETER_NUM		8	/* parameter文件的备份个数 */
+#define PARAMETER_OFFSET	1024	/* 每个parameter的偏移量 */
 
-#define MAX_LINE_CHAR			(1024*64)	        // Parameters有多个Line组成，限制每个Line最大占1024 Bytes
-#define MAX_LOADER_PARAM		(128*512)		// Parameters所占的最大Sector数(含tag、length、crc等)
-#define PARM_TAG			0x4D524150
-#define MAGIC_CODE			0x00280028
-#define EATCHAR(x, c)			for (; *(x) == (c); (x)++) ; // 去除字符串x中左边为c的字符
-#define PART_NAME	32
-#define MAX_MTDID	64
+#define MAX_LINE_CHAR		(1024*64) /* Parameters有多个Line组成，限制每个Line最大占1024 Bytes */
+#define MAX_LOADER_PARAM	(128*512) /* Parameters所占的最大Sector数(含tag、length、crc等) */
+#define PARM_TAG		0x4D524150
+#define MAGIC_CODE		0x00280028
+#define EATCHAR(x, c)		for (; *(x) == (c); (x)++) ; /* 去除字符串x中左边为c的字符 */
+#define PART_NAME		32
+#define MAX_MTDID		64
 
-#define MTD_WRITEABLE			0x400	/* Device is writeable */
-#define MTD_POWERUP_LOCK		0x2000	/* Always locked after reset */
-
-
-#define SIZE_REMAINING			0xFFFFFFFF
+#define MTD_WRITEABLE		0x400	/* Device is writeable */
+#define MTD_POWERUP_LOCK	0x2000	/* Always locked after reset */
 
 
-#define PARTNAME_MISC			"misc"
-#define PARTNAME_KERNEL			"kernel"
-#define PARTNAME_BOOT			"boot"
-#define PARTNAME_RECOVERY		"recovery"
-#define PARTNAME_SYSTEM		    	"system"
-#define PARTNAME_BACKUP			"backup"
-#define PARTNAME_SNAPSHOT		"snapshot"
+#define SIZE_REMAINING		0xFFFFFFFF
+
+
+#define PARTNAME_MISC		"misc"
+#define PARTNAME_KERNEL		"kernel"
+#define PARTNAME_BOOT		"boot"
+#define PARTNAME_RECOVERY	"recovery"
+#define PARTNAME_SYSTEM		"system"
+#define PARTNAME_BACKUP		"backup"
+#define PARTNAME_SNAPSHOT	"snapshot"
 
 typedef struct tag_cmdline_mtd_partition {
 	char mtd_id[MAX_MTDID];
@@ -39,30 +39,28 @@ typedef struct tag_cmdline_mtd_partition {
 	disk_partition_t parts[CONFIG_MAX_PARTITIONS];
 } cmdline_mtd_partition;
 
-typedef struct tagLoaderParam
-{
+typedef struct tagLoaderParam {
 	uint32	tag;
 	uint32	length;
 	char	parameter[1];
 	uint32	crc;
 } LoaderParam, *PLoaderParam;
 
-typedef struct tagBootInfo
-{
+typedef struct tagBootInfo {
 	uint32 magic_code;
 	uint16 machine_type;
-	uint16 boot_index;		// 0 - normal boot, 1 - recovery
+	uint16 boot_index; /* 0 - normal boot, 1 - recovery */
 	uint32 atag_addr;
 	uint32 misc_offset;
 	uint32 kernel_load_addr;
-	uint32 boot_offset;		// 以Sector为单位
-	uint32 recovery_offset;		// 以Sector为单位
-	uint32 ramdisk_offset;	// 以Sector为单位
-	uint32 ramdisk_size;	// 以Byte为单位
+	uint32 boot_offset; /* 以Sector为单位 */
+	uint32 recovery_offset; /* 以Sector为单位 */
+	uint32 ramdisk_offset; /* 以Sector为单位 */
+	uint32 ramdisk_size; /* 以Byte为单位 */
 	uint32 ramdisk_load_addr;
 	uint32 is_kernel_in_boot;
-	
-	uint32 check_mask;	// 00 - 不校验， 01 - check kernel, 10 - check ramdisk, 11 - both check
+
+	uint32 check_mask; /* 00 - 不校验， 01 - check kernel, 10 - check ramdisk, 11 - both check */
 	char cmd_line[MAX_LINE_CHAR];
 	cmdline_mtd_partition cmd_mtd;
 
@@ -79,7 +77,6 @@ typedef struct tagBootInfo
 
 
 extern BootInfo			gBootInfo;
-extern uint32			parameter_lba;
 
 extern void ParseParam(PBootInfo pboot_info, char *param, uint32 len);
 extern int32 GetParam(uint32 param_addr, void* buf);
