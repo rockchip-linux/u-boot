feb7f8259fefb79eca0a6c3997862c1d8ff9b2e6
cwz <cwz@rock-chips.com>
2015-12-07 21:02:28 +0800

    rk plat: fix make shell spelling error.
    Change-Id: Id0d255e7b3dbf8b33ceeda603ae7b5c49c9c138e
    Signed-off-by: cwz <cwz@rock-chips.com>

--- mkv7.sh
+++ mkv7.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo    "******************************"
-echo    "*  Make armv8 aarch32 Uboot  *"
+echo    "*     Make AArch32 Uboot     *"
 echo    "******************************"
 make --jobs=`sed -n "N;/processor/p" /proc/cpuinfo|wc -l`
 
