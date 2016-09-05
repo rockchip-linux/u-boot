rockchip-linux/u-boot

commit 1db6abe4ef47cc37cf39142220710d788d3e369b

video: enable new rockchip display
Change-Id: Id96360154c85613f27cf430bccb95bd2fd98991d
Signed-off-by: Mark Yao <mark.yao@rock-chips.com>

board/rockchip/common/rkboot/fastboot.c
@@ -31,15 +31,22 @@ extern int rk_pwm_bl_config(int brightness);
 #endif
 #ifdef CONFIG_LCD
 extern int drv_lcd_init(void);
+#ifdef CONFIG_RK_FB
 extern void lcd_standby(int enable);
+#endif
 #endif
 
 #if defined(CONFIG_RK_PWM_REMOTE)
 extern int g_ir_keycode;
 #endif
 
-#ifdef CONFIG_LCD
+#if defined(CONFIG_LCD) || defined(CONFIG_ROCKCHIP_DISPLAY)
 int g_logo_on_state = 0;
+int g_is_new_display = 0;
+#endif
+#ifdef CONFIG_ROCKCHIP_DISPLAY
+extern int rockchip_display_init(void);
+extern void rockchip_show_logo(void);
 #endif
 
 #ifdef CONFIG_UBOOT_CHARGE
@@ -373,6 +380,7 @@ static void board_fbt_low_power_off(void)
 		if (!is_charging()) {
 			FBTERR("low power, shutting down...\n");
 #ifdef CONFIG_LCD
+#ifdef CONFIG_RK_FB
 			//TODO: show warning logo.
 			show_resource_image("images/battery_fail.bmp");
 
@@ -389,6 +397,7 @@ static void board_fbt_low_power_off(void)
 			rk_pwm_bl_config(0);
 #endif
 			lcd_standby(1);
+#endif
 #endif
 			shut_down();
 			printf("not reach here.\n");
@@ -441,21 +450,37 @@ void board_fbt_preboot(void)
 	board_fbt_low_power_check();
 #endif
 
-#ifdef CONFIG_LCD
+#if defined(CONFIG_LCD) || defined(CONFIG_ROCKCHIP_DISPLAY)
 	/* logo state defautl init = 0 */
 	g_logo_on_state = 0;
+	g_is_new_display = 1;
 
 	if (gd->fdt_blob) {
-		int node = fdt_path_offset(gd->fdt_blob, "/fb");
-		g_logo_on_state = fdtdec_get_int(gd->fdt_blob, node, "rockchip,uboot-logo-on", 0);
-	}
-	gd->uboot_logo = g_logo_on_state;
-	printf("read logo on state from dts [%d]\n", g_logo_on_state);
+		int node = fdt_path_offset(gd->fdt_blob, "/display-subsystem");
 
-	if (g_logo_on_state != 0) {
-		lcd_enable_logo(true);
-		drv_lcd_init();
+		if (node < 0) {
+#if defined(CONFIG_LCD)
+			g_is_new_display = 0;
+			node = fdt_path_offset(gd->fdt_blob, "/fb");
+			g_logo_on_state = fdtdec_get_int(gd->fdt_blob, node, "rockchip,uboot-logo-on", 0);
+			if (g_logo_on_state != 0) {
+				lcd_enable_logo(true);
+				drv_lcd_init();
+			}
+#else
+			printf("failed to found display node\n");
+#endif
+		}
+#if defined(CONFIG_ROCKCHIP_DISPLAY)
+		else if (!rockchip_display_init()) {
+			g_logo_on_state = 1;
+		}
+#endif
 	}
+
+	gd->uboot_logo = g_logo_on_state;
+
+	printf("read logo on state from dts [%d]\n", g_logo_on_state);
 #endif
 
 #ifdef CONFIG_RK_POWER
@@ -500,13 +525,27 @@ void board_fbt_preboot(void)
 	powerOn();
 #endif
 
-#ifdef CONFIG_LCD
+#if defined(CONFIG_LCD) || defined(CONFIG_ROCKCHIP_DISPLAY)
 	if (g_logo_on_state != 0) {
-		lcd_standby(0);
+#ifdef CONFIG_ROCKCHIP_DISPLAY
+		if (g_is_new_display) {
+			rockchip_show_logo();
 #ifdef CONFIG_RK_PWM_BL
-		/* use defaut brightness in dts */
-		rk_pwm_bl_config(-1);
+			rk_pwm_bl_config(-1);
 #endif
+		} else
+#endif
+		{
+#ifdef CONFIG_LCD
+#ifdef CONFIG_RK_FB
+			lcd_standby(0);
+#ifdef CONFIG_RK_PWM_BL
+			/* use defaut brightness in dts */
+			rk_pwm_bl_config(-1);
+#endif
+#endif
+#endif
+		}
 	}
 #endif
 

arch/arm/lib/board.c
@@ -344,7 +344,7 @@ void board_init_f(ulong bootflag)
 	addr &= ~(4096 - 1);
 	debug("Top of RAM usable for U-Boot at: %08lx\n", addr);
 
-#ifdef CONFIG_LCD
+#if defined(CONFIG_LCD) || defined(CONFIG_ROCKCHIP_DISPLAY)
 
 	/* if defind CONFIG_RK_FB_SIZE, set fb base at the end of ddr address */
 #if defined(CONFIG_ROCKCHIP) && defined(CONFIG_RK_FB_DDREND)

common/board_f.c
@@ -456,7 +456,7 @@ static int reserve_mmu(void)
 }
 #endif
 
-#ifdef CONFIG_LCD
+#if defined(CONFIG_LCD) || defined(CONFIG_ROCKCHIP_DISPLAY)
 static int reserve_lcd(void)
 {
 	/* if defind CONFIG_RK_FB_SIZE, set fb base at the end of ddr address */
@@ -989,7 +989,7 @@ static init_fnc_t init_sequence_f[] = {
 		defined(CONFIG_ARM)
 	reserve_mmu,
 #endif
-#ifdef CONFIG_LCD
+#if defined(CONFIG_LCD) || defined(CONFIG_ROCKCHIP_DISPLAY)
 	reserve_lcd,
 #endif
 #ifdef CONFIG_ROCKCHIP

common/cmd_bootrk.c
@@ -15,8 +15,9 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#ifdef CONFIG_LCD
+#if defined(CONFIG_LCD) || defined(CONFIG_ROCKCHIP_DISPLAY)
 extern int g_logo_on_state;
+extern int g_is_new_display;
 #endif
 #ifdef CONFIG_UBOOT_CHARGE
 extern int android_charge_mode;
@@ -544,7 +545,7 @@ int do_bootrk(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	}
 #if defined(CONFIG_LCD) && defined(CONFIG_KERNEL_LOGO)
 	/* if uboot logo enable, load logo */
-	if (g_logo_on_state != 0) {
+	if (g_logo_on_state != 0 && !g_is_new_display) {
 		rk_load_kernel_logo();
 	}
 #endif

include/asm-generic/global_data.h
@@ -37,7 +37,7 @@ typedef struct global_data {
 	/* We cannot bracket this with CONFIG_PCI due to mpc5xxx */
 	unsigned long pci_clk;
 	unsigned long mem_clk;
-#if defined(CONFIG_LCD) || defined(CONFIG_VIDEO)
+#if defined(CONFIG_LCD) || defined(CONFIG_VIDEO) || defined(CONFIG_ROCKCHIP_DISPLAY)
 	unsigned long fb_base;	/* Base address of framebuffer mem */
 #endif
 #if defined(CONFIG_POST) || defined(CONFIG_LOGBUFFER)

commit 28b53a20ca448e461f96758db80e1c95a7f04b41

video: rockchip: fb: fix old display driver will not be load
Change-Id: I70d7d171afa6d8c27a9efad254f4850cc1b50cb2
Fixes: 1db6abe4ef47c ("video: enable new rockchip display")
Signed-off-by: Huang Jiachai <hjc@rock-chips.com>

board/rockchip/common/rkboot/fastboot.c
@@ -458,7 +458,7 @@ void board_fbt_preboot(void)
 	if (gd->fdt_blob) {
 		int node = fdt_path_offset(gd->fdt_blob, "/display-subsystem");
 
-		if (node < 0) {
+		if (!fdt_device_is_available(gd->fdt_blob, node)) {
 #if defined(CONFIG_LCD)
 			g_is_new_display = 0;
 			node = fdt_path_offset(gd->fdt_blob, "/fb");