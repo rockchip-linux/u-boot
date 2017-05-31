ec575bfc3fd8ebb19a9a737a696f35d76b9e25d4
chenjh <chenjh@rock-chips.com>
2017-05-31 10:09:11 +0800

    bootm-fdt: rockchip: arch_fixup_fdt supports LPAE
    the old version set memory not larger than CONFIG_MAX_MEM_ADDR.
    
    Change-Id: I9a7a9d73d0c95ce622ba2f09c613480ffc989dd8
    Signed-off-by: chenjh <chenjh@rock-chips.com>

--- arch/arm/lib/bootm-fdt.c
+++ arch/arm/lib/bootm-fdt.c
@@ -29,22 +29,62 @@ int arch_fixup_fdt(void *blob)
 #if defined(CONFIG_ROCKCHIP) && defined(CONFIG_RK_MAX_DRAM_BANKS)
 	u64 _start[CONFIG_RK_MAX_DRAM_BANKS];
 	u64 _size[CONFIG_RK_MAX_DRAM_BANKS];
-	for (bank = 0; bank < CONFIG_RK_MAX_DRAM_BANKS; bank++) {
-		if (!bd->rk_dram[bank].size)
+	int rk_bank;
+	for (bank = 0, rk_bank = 0; bank < CONFIG_RK_MAX_DRAM_BANKS; bank++, rk_bank++) {
+		if (!bd->rk_dram[rk_bank].size)
 			break;
-		_start[bank] = bd->rk_dram[bank].start;
-		_size[bank] = bd->rk_dram[bank].size;
+		_start[bank] = bd->rk_dram[rk_bank].start;
+		_size[bank] = bd->rk_dram[rk_bank].size;
 #ifdef CONFIG_MAX_MEM_ADDR
+/*
+ * |--------------------|---------------|---------------------------|
+ * |			|     iomap	|			..........
+ * |--------O-----------|-------O---O---|-------------O------O------|
+ * 0x0	    A	    MAX_MEM	B   C	4GB	      D	     E
+ */
+		#define MEM_4GB_BASE 0x100000000
+
+		u64 _splited_bank2_size;
+
 		if (_start[bank] < CONFIG_MAX_MEM_ADDR) {
-			if ((_start[bank] + _size[bank]) >= CONFIG_MAX_MEM_ADDR) {
-				//resize bank.
+			/* A-B */
+			if (((_start[bank] + _size[bank]) >= CONFIG_MAX_MEM_ADDR) &&
+			    ((_start[bank] + _size[bank]) <= MEM_4GB_BASE)) {
+				/* resize bank */
 				_size[bank] = CONFIG_MAX_MEM_ADDR - _start[bank];
+			/* A-D: cover iomap area, split one bank into two banks */
+			} else if ((_start[bank] + _size[bank]) > MEM_4GB_BASE) {
+				/* pre calculate before update _size[bank] */
+				_splited_bank2_size = _start[bank] + _size[bank] - MEM_4GB_BASE;
+
+				/* splited bank1 */
+				_size[bank] = CONFIG_MAX_MEM_ADDR - _start[bank];
+				printf("Add bank:%016llx, %016llx\n", _start[bank], _size[bank]);
+
+				/* splited-bank2 */
+				bank++;
+				_start[bank] = MEM_4GB_BASE;
+				_size[bank] = _splited_bank2_size;
+			}
+		} else if ((_start[bank] > CONFIG_MAX_MEM_ADDR) && (_start[bank] < MEM_4GB_BASE)) {
+			/* B-C */
+			if (_start[bank] + _size[bank] <= MEM_4GB_BASE) {
+				_start[bank] = 0;
+				_size[bank] = 0;
+				/* because 'for()' will do bank++,
+				 * but we don't want this. So we do bank--
+				 * to counteract.
+				 */
+				bank--;
+			/* B-D */
+			} else {
+				_size[bank] = _start[bank] + _size[bank] - MEM_4GB_BASE;
+				_start[bank] = MEM_4GB_BASE;
 			}
 		} else {
-			_start[bank] = CONFIG_MAX_MEM_ADDR;
-			_size[bank] = 0;
+			/* D-E: do nothing */
 		}
-#endif
+#endif	/* CONFIG_MAX_MEM_ADDR */
 		printf("Add bank:%016llx, %016llx\n", _start[bank], _size[bank]);
 	}
 	if (bank) {
