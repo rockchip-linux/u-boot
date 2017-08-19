3f071030665b1022866d5b118a1fff4c2c400d60
Kamil Trzcinski <ayufan@ayufan.eu>
2017-08-20 01:52:34 +0200

    Add rk3328-efuse support

--- drivers/misc/rockchip-efuse.c
+++ drivers/misc/rockchip-efuse.c
@@ -16,6 +16,14 @@
 #include <linux/delay.h>
 #include <misc.h>
 
+#define RK3328_INT_CON	0x0014
+#define RK3328_INT_STATUS	0x0018
+#define RK3328_DOUT		0x0020
+#define RK3328_AUTO_CTRL	0x0024
+#define RK3328_INT_FINISH	BIT(0)
+#define RK3328_AUTO_ENB		BIT(0)
+#define RK3328_AUTO_RD		BIT(1)
+
 #define RK3399_A_SHIFT          16
 #define RK3399_A_MASK           0x3ff
 #define RK3399_NFUSES           32
@@ -94,6 +102,49 @@ U_BOOT_CMD(
 	""
 );
 #endif
+
+static int rockchip_rk3328_efuse_read(struct udevice *dev, int offset,
+				      void *buf, int size)
+{
+	struct rockchip_efuse_platdata *plat = dev_get_platdata(dev);
+
+	unsigned int addr_start, addr_end, addr_offset;
+	u32 out_value, status;
+	u8  bytes[RK3399_NFUSES * RK3399_BYTES_PER_FUSE];
+	int i = 0;
+	u32 addr;
+
+	/* 128 Byte efuse, 96 Byte for secure, 32 Byte for non-secure */
+	offset += 96;
+
+	addr_start = offset / RK3399_BYTES_PER_FUSE;
+	addr_offset = offset % RK3399_BYTES_PER_FUSE;
+	addr_end = DIV_ROUND_UP(offset + size, RK3399_BYTES_PER_FUSE);
+
+	/* cap to the size of the efuse block */
+	if (addr_end > RK3399_NFUSES)
+		addr_end = RK3399_NFUSES;
+
+	for (addr = addr_start; addr < addr_end; addr++) {
+		writel(RK3328_AUTO_RD | RK3328_AUTO_ENB |
+		       ((addr & RK3399_A_MASK) << RK3399_A_SHIFT),
+		       plat->base + RK3328_AUTO_CTRL);
+		udelay(10);
+		status = readl(plat->base + RK3328_INT_STATUS);
+		if (!(status & RK3328_INT_FINISH)) {
+			return -EIO;
+		}
+		out_value = readl(plat->base + RK3328_DOUT);
+		writel(RK3328_INT_FINISH, plat->base + RK3328_INT_STATUS);
+
+		memcpy(&bytes[i], &out_value, RK3399_BYTES_PER_FUSE);
+		i += RK3399_BYTES_PER_FUSE;
+	}
+
+	memcpy(buf, bytes + addr_offset, size);
+
+	return 0;
+}
 
 static int rockchip_rk3399_efuse_read(struct udevice *dev, int offset,
 				      void *buf, int size)
@@ -222,6 +273,10 @@ static const struct udevice_id rockchip_efuse_ids[] = {
 	{
 		.compatible = "rockchip,rk322x-efuse",
 		.data = (ulong)&rockchip_rk3288_efuse_read,
+	},
+	{
+		.compatible = "rockchip,rk3328-efuse",
+		.data = (ulong)rockchip_rk3328_efuse_read,
 	},
 	{
 		.compatible = "rockchip,rk3399-efuse",
