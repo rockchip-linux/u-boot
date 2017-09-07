4adc4905b77515a64d05c9739cf4dd27da9cdaec
Wyon Bi <bivvy.bi@rock-chips.com>
2017-09-07 14:48:58 +0800

    video/rockchip: panel: update support panel list
    - LG LM238WR2-SPA1
    - LG LM270WR3-SSA1
    - BOE MV238QUM-N20
    
    Change-Id: I3be9ce391181c5446f5d3e346512a657569cc96f
    Signed-off-by: Wyon Bi <bivvy.bi@rock-chips.com>

--- drivers/video/rockchip_panel.c
+++ drivers/video/rockchip_panel.c
@@ -34,6 +34,48 @@ static const struct drm_display_mode auo_b125han03_mode = {
 	.flags = DRM_MODE_FLAG_NVSYNC | DRM_MODE_FLAG_NHSYNC,
 };
 
+static const struct drm_display_mode boe_mv238qum_n20_mode = {
+	.clock = 559440,
+	.hdisplay = 3840,
+	.hsync_start = 3840 + 150,
+	.hsync_end = 3840 + 150 + 60,
+	.htotal = 3840 + 150 + 60 + 150,
+	.vdisplay = 2160,
+	.vsync_start = 2160 + 24,
+	.vsync_end = 2160 + 24 + 12,
+	.vtotal = 2160 + 24 + 12 + 24,
+	.vrefresh = 60,
+	.flags = DRM_MODE_FLAG_NVSYNC | DRM_MODE_FLAG_NHSYNC,
+};
+
+static const struct drm_display_mode lg_lm238wr2_spa1_mode = {
+	.clock = 533250,
+	.hdisplay = 3840,
+	.hsync_start = 3840 + 48,
+	.hsync_end = 3840 + 48 + 32,
+	.htotal = 3840 + 48 + 32 + 80,
+	.vdisplay = 2160,
+	.vsync_start = 2160 + 3,
+	.vsync_end = 2160 + 3 + 5,
+	.vtotal = 2160 + 3 + 5 + 54,
+	.vrefresh = 60,
+	.flags = DRM_MODE_FLAG_NVSYNC | DRM_MODE_FLAG_NHSYNC,
+};
+
+static const struct drm_display_mode lg_lm270wr3_ssa1_mode = {
+	.clock = 533250,
+	.hdisplay = 3840,
+	.hsync_start = 3840 + 48,
+	.hsync_end = 3840 + 48 + 32,
+	.htotal = 3840 + 48 + 32 + 80,
+	.vdisplay = 2160,
+	.vsync_start = 2160 + 3,
+	.vsync_end = 2160 + 3 + 5,
+	.vtotal = 2160 + 3 + 5 + 54,
+	.vrefresh = 60,
+	.flags = DRM_MODE_FLAG_NVSYNC | DRM_MODE_FLAG_NHSYNC,
+};
+
 static const struct drm_display_mode lg_lp079qx1_sp0v_mode = {
 	.clock = 200000,
 	.hdisplay = 1536,
@@ -56,13 +98,25 @@ static const struct rockchip_panel g_panel[] = {
 		.compatible = "simple-panel-dsi",
 		.funcs = &rockchip_dsi_panel_funcs,
 	}, {
-		.compatible = "lg,lp079qx1-sp0v",
-		.funcs = &panel_simple_funcs,
-		.data = &lg_lp079qx1_sp0v_mode,
-	}, {
 		.compatible = "auo,b125han03",
 		.funcs = &panel_simple_funcs,
 		.data = &auo_b125han03_mode,
+	}, {
+		.compatible = "boe,mv238qum-n20",
+		.funcs = &panel_simple_funcs,
+		.data = &boe_mv238qum_n20_mode,
+	}, {
+		.compatible = "lg,lm238wr2-spa1",
+		.funcs = &panel_simple_funcs,
+		.data = &lg_lm238wr2_spa1_mode,
+	}, {
+		.compatible = "lg,lm270wr3-ssa1",
+		.funcs = &panel_simple_funcs,
+		.data = &lg_lm270wr3_ssa1_mode,
+	}, {
+		.compatible = "lg,lp079qx1-sp0v",
+		.funcs = &panel_simple_funcs,
+		.data = &lg_lp079qx1_sp0v_mode,
 	},
 };
 
