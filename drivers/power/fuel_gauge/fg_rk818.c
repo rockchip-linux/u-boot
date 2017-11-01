cd572be1da845aa21d50f8bac7af9f353497a989
Joseph Chen <chenjh@rock-chips.com>
2017-11-01 16:24:35 +0800

    fuel gauge: rk818: do rsoc initialization when first power on
    clean up logic of rsoc initialization conditions.
    
    Change-Id: I822ff0db82df416dea1b32917c8af7e30a9749ca
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- drivers/power/fuel_gauge/fg_rk818.c
+++ drivers/power/fuel_gauge/fg_rk818.c
@@ -864,8 +864,35 @@ static bool rk818_bat_ocv_sw_reset(struct battery_info *di)
 
 void rk818_bat_init_rsoc(struct battery_info *di)
 {
-	di->pwroff_min = rk818_bat_get_pwroff_min(di);
+	int charger, voltage, uboot_charge = 0, initialize = 0;
+
+#ifdef CONFIG_UBOOT_CHARGE
+	uboot_charge = 1;
+#endif
+	charger = rk818_bat_get_usb_state(di);
+	voltage = rk818_bat_get_est_voltage(di);
 	di->is_first_power_on = is_rk818_bat_first_poweron(di);
+
+	/*
+	 * Do rsoc initialization only when:
+	 *
+	 * 1. first power on;
+	 * 2. charger online + voltage lower than CONFIG_SCREEN_ON_VOL_THRESD;
+	 * 3. charger online + CONFIG_UBOOT_CHARGE enabled.
+	 */
+	if (di->is_first_power_on) {
+		initialize = 1;
+	} else if (charger != NO_CHARGER) {
+		if (voltage < CONFIG_SCREEN_ON_VOL_THRESD)
+			initialize = 1;
+		else if (uboot_charge)
+			initialize = 1;
+	}
+
+	if (!initialize)
+		return;
+
+	di->pwroff_min = rk818_bat_get_pwroff_min(di);
 	di->is_sw_reset = rk818_bat_ocv_sw_reset(di);
 
 	if (di->is_first_power_on || di->is_sw_reset)
@@ -968,17 +995,7 @@ static void rk818_bat_fg_init(struct battery_info *di)
 	rk818_bat_clr_initialized_state(di);
 	di->dsoc = rk818_bat_get_dsoc(di);
 
-	/* it's better to init fg in kernel,
-	 * so avoid init in uboot as far as possible*/
-	if (rk818_bat_get_usb_state(di) != NO_CHARGER) {
-		if (rk818_bat_get_est_voltage(di) < CONFIG_SCREEN_ON_VOL_THRESD)
-			rk818_bat_init_rsoc(di);
-#ifdef CONFIG_UBOOT_CHARGE
-		else
-			rk818_bat_init_rsoc(di);
-#endif
-	}
-
+	rk818_bat_init_rsoc(di);
 	rk818_bat_init_chrg_config(di);
 	di->voltage_avg = rk818_bat_get_avg_voltage(di);
 	di->voltage_ocv = rk818_bat_get_ocv_voltage(di);
