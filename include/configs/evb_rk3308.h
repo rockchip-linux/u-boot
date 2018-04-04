ccd22f22f619dd8765a21e68b94b73bea43eff08
Sandy Huang <hjc@rock-chips.com>
2018-04-04 09:19:34 +0800

    drm/rockchip: vop: add support rk3308 vop
    Change-Id: I067ac3f95163dd678317a11ffad7a7d85225c9ee
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- include/configs/evb_rk3308.h
+++ include/configs/evb_rk3308.h
@@ -8,8 +8,16 @@
 
 #include <configs/rk3308_common.h>
 
+#define CONFIG_SYS_MMC_ENV_DEV 0
+
+
 #define ROCKCHIP_DEVICE_SETTINGS \
 			"stdout=serial,vidconsole\0" \
 			"stderr=serial,vidconsole\0"
+#undef CONFIG_CONSOLE_SCROLL_LINES
+#define CONFIG_CONSOLE_SCROLL_LINES            10
+
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND RKIMG_BOOTCOMMAND
 
 #endif
