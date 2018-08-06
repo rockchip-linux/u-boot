ba1f94309a0644088f7f1ab1a78c747b9fc04f55
Kamil Trzciński <ayufan@ayufan.eu>
2018-08-06 22:45:01 +0200

    dev.mk: allow to erase bootloader.sh
    Change-Id: I103be0655bff0c7950bb2a20aa9c9e6443518b7f

---
+++ dev-ayufan/root/usr/local/sbin/rock64_erase_bootloader.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source /usr/local/lib/u-boot-rockchip/loader-common
+
+echo "Doing this will erase bootloader stored on your boot device"
+echo "  and it might break your system."
+echo "Use this only if you prefer to boot from SPI ('rock64_write_spi_flash.sh')."
+echo "To bring back bootloader run 'rock64_upgrade_bootloader.sh'."
+echo ""
+
+confirm
+
+MNT_DEV=$(findmnt /boot/efi -n -o SOURCE)
+
+write_sd() {
+    case "$1" in
+        /dev/mmcblk*p6|/dev/sd*p6|/dev/mapper/loop*p6|/dev/mapper/nvme*p6)
+            dd if="$2" of="${1/p6/p1}"
+            ;;
+
+        *)
+            echo "Cannot detect boot device ($MNT_DEV)."
+            exit 1
+            ;;
+    esac
+}
+
+write_sd "$MNT_DEV" "/dev/zero"
+
+sync
+
+echo Done.
