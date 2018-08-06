a851a05b0f53dfb02e650dea663f23ad9fd5d673
Kamil Trzciński <ayufan@ayufan.eu>
2018-08-06 21:02:41 +0200

    dev.mk: update bootloader upgrade scripts
    Change-Id: Ib319cb0604398482578037479bf223e11cc489e0

--- dev-ayufan/root/usr/local/sbin/rock64_upgrade_bootloader.sh
+++ dev-ayufan/root/usr/local/sbin/rock64_upgrade_bootloader.sh
@@ -2,67 +2,31 @@
 
 set -eo pipefail
 
-if [[ "$(id -u)" -ne "0" ]]; then
-    echo "This script requires root."
-    exit 1
-fi
-
-if dpkg -s "u-boot-rockchip-rockpro64" &>/dev/null; then
-    BOARD=rockpro64
-    LOADER_NAME=rksd_loader
-elif dpkg -s "u-boot-rockchip-rock64" &>/dev/null; then
-    BOARD=rock64
-    LOADER_NAME=rksd_loader
-else
-    exit "Unknown package installed."
-    exit 1
-fi
-
-LOADER="/usr/lib/u-boot-${BOARD}/${LOADER_NAME}.img"
-if [[ ! -f "$LOADER" ]]; then
-    echo "Missing board bootloader image: $LOADER"
-    exit 1
-fi
+source /usr/local/lib/u-boot-rockchip/loader-common
 
 echo "Doing this will overwrite bootloader stored on your boot device it might break your system."
 echo "If this happens you will have to manually fix that outside of your Rock64."
 echo "If you are booting from SPI. You have to use 'rock64_write_spi_flash.sh'."
 echo ""
 
-if ! grep -qi "$BOARD" /proc/device-tree/compatible; then
-    echo "You are currently running on different board:"
-    cat /proc/device-tree/model
-    echo "It may brick your device or the system unless"
-    echo "you know what are you doing."
-    echo ""
-fi
-
-while true; do
-    echo "Type YES to continue or Ctrl-C to abort."
-    read CONFIRM
-    if [[ "$CONFIRM" == "YES" ]]; then
-        break
-    fi
-done
-
-if ! debsums -s "u-boot-rockchip-${BOARD}"; then
-    echo "Verification of 'u-boot-rockchip-${BOARD}' failed."
-    echo "Your disk might have got corrupted."
-    exit 1
-fi
+confirm
 
 MNT_DEV=$(findmnt /boot/efi -n -o SOURCE)
 
-case $MNT_DEV in
-    /dev/mmcblk*p6|/dev/sd*p6|/dev/mapper/loop*p6|/dev/mapper/nvme*p6)
-        dd if=$LOADER of="${MNT_DEV/p6/p1}"
-        ;;
+write_sd() {
+    case "$1" in
+        /dev/mmcblk*p6|/dev/sd*p6|/dev/mapper/loop*p6|/dev/mapper/nvme*p6)
+            dd if="$2" of="${1/p6/p1}"
+            ;;
 
-    *)
-        echo "Cannot detect boot device ($MNT_DEV)."
-        exit 1
-        ;;
-esac
+        *)
+            echo "Cannot detect boot device ($MNT_DEV)."
+            exit 1
+            ;;
+    esac
+}
+
+write_sd "$MNT_DEV" "$SD_LOADER"
 
 sync
 
--- dev-ayufan/root/usr/local/sbin/rock64_write_spi_flash.sh
+++ dev-ayufan/root/usr/local/sbin/rock64_write_spi_flash.sh
@@ -2,59 +2,14 @@
 
 set -eo pipefail
 
-if [[ "$(id -u)" -ne "0" ]]; then
-    echo "This script requires root."
-    exit 1
-fi
-
-if ! which nandwrite &>/dev/null; then
-    echo "Install mtd-utils with 'apt-get install mtd-utils'"
-    exit 1
-fi
-
-if dpkg -s "u-boot-rockchip-rockpro64" &>/dev/null; then
-    BOARD=rockpro64
-    LOADER_NAME=rkspi_loader
-elif dpkg -s "u-boot-rockchip-rock64" &>/dev/null; then
-    BOARD=rock64
-    LOADER_NAME=rksd_loader
-else
-    exit "Unknown package installed."
-    exit 1
-fi
-
-LOADER="/usr/lib/u-boot-${BOARD}/${LOADER_NAME}.img"
-if ! -f "$LOADER"; then
-    echo "Missing board bootloader image: $LOADER"
-    exit 1
-fi
+source /usr/local/lib/u-boot-rockchip/loader-common
 
 echo "Doing this will overwrite data stored on SPI Flash"
 echo "  and it will require that you use eMMC or SD"
 echo "  as your boot device."
 echo ""
 
-if ! grep -qi "$BOARD" /proc/device-tree/compatible; then
-    echo "You are currently running on different board:"
-    cat /proc/device-tree/model
-    echo "It may brick your device or the system unless"
-    echo "you know what are you doing."
-    echo ""
-fi
-
-while true; do
-    echo "Type YES to continue or Ctrl-C to abort."
-    read CONFIRM
-    if [[ "$CONFIRM" == "YES" ]]; then
-        break
-    fi
-done
-
-if ! debsums -s "u-boot-rockchip-${BOARD}"; then
-    echo "Verification of 'u-boot-rockchip-${BOARD}' failed."
-    echo "Your disk might have got corrupted."
-    exit 1
-fi
+confirm
 
 MNT_DEV=$(findmnt /boot/efi -n -o SOURCE)
 
@@ -69,6 +24,6 @@ write_nand() {
     nandwrite "/dev/$MTD" < "$2"
 }
 
-write_nand loader "$LOADER"
+write_nand loader "$SPI_LOADER"
 
 echo Done.
