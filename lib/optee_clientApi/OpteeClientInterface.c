9632425b78f2838da5a53ea84f23ca0a50c61340
Tony Xu <tony.xu@rock-chips.com>
2018-09-21 09:53:13 +0800

    lib: optee_clientApi: add an API to write keybox to secure storage
    Change-Id: Ib8d541c6a4776d0d8e1e10a5372c1c76b59e9e20
    Signed-off-by: Tony Xu <tony.xu@rock-chips.com>

--- lib/optee_clientApi/OpteeClientInterface.c
+++ lib/optee_clientApi/OpteeClientInterface.c
@@ -9,7 +9,136 @@
 #include <optee_include/OpteeClientApiLib.h>
 #include <optee_include/tee_client_api.h>
 
-#define     BOOT_FROM_EMMC    (1<<1)
+#define	BOOT_FROM_EMMC	(1<<1)
+#define	WIDEVINE_TAG	"KBOX"
+#define	ATTESTATION_TAG	"ATTE"
+
+uint32_t rk_send_keybox_to_ta(uint8_t *filename, uint32_t filename_size,
+			      TEEC_UUID uuid,
+			      uint8_t *key, uint32_t key_size,
+			      uint8_t *data, uint32_t data_size)
+{
+	TEEC_Result TeecResult;
+	TEEC_Context TeecContext;
+	TEEC_Session TeecSession;
+	uint32_t ErrorOrigin;
+	TEEC_UUID *TeecUuid = &uuid;
+	TEEC_Operation TeecOperation = {0};
+	TEEC_SharedMemory SharedMem0 = {0};
+	TEEC_SharedMemory SharedMem1 = {0};
+	TEEC_SharedMemory SharedMem2 = {0};
+
+	OpteeClientApiLibInitialize();
+	TeecResult = TEEC_InitializeContext(NULL, &TeecContext);
+	TeecOperation.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,
+						    TEEC_NONE,
+						    TEEC_NONE,
+						    TEEC_NONE);
+
+	/* 0 nand or emmc "security" partition , 1 rpmb */
+	if (StorageGetBootMedia() == BOOT_FROM_EMMC)
+		TeecOperation.params[0].value.a = 1;
+	else
+		TeecOperation.params[0].value.a = 0;
+#ifdef CONFIG_OPTEE_ALWAYS_USE_SECURITY_PARTITION
+        TeecOperation.params[0].value.a = 0;
+#endif
+
+	TeecResult = TEEC_OpenSession(&TeecContext,
+				      &TeecSession,
+				      TeecUuid,
+				      TEEC_LOGIN_PUBLIC,
+				      NULL,
+				      &TeecOperation,
+				      &ErrorOrigin);
+
+	SharedMem0.size = filename_size;
+	SharedMem0.flags = 0;
+	TeecResult = TEEC_AllocateSharedMemory(&TeecContext, &SharedMem0);
+	memcpy(SharedMem0.buffer, filename, SharedMem0.size);
+
+	SharedMem1.size = key_size;
+	SharedMem1.flags = 0;
+	TeecResult = TEEC_AllocateSharedMemory(&TeecContext, &SharedMem1);
+	memcpy(SharedMem1.buffer, key, SharedMem1.size);
+
+	SharedMem2.size = data_size;
+	SharedMem2.flags = 0;
+	TeecResult = TEEC_AllocateSharedMemory(&TeecContext, &SharedMem2);
+	memcpy(SharedMem2.buffer, data, SharedMem2.size);
+
+	TeecOperation.params[0].tmpref.buffer = SharedMem0.buffer;
+	TeecOperation.params[0].tmpref.size = SharedMem0.size;
+	TeecOperation.params[1].tmpref.buffer = SharedMem1.buffer;
+	TeecOperation.params[1].tmpref.size = SharedMem1.size;
+	TeecOperation.params[2].tmpref.buffer = SharedMem2.buffer;
+	TeecOperation.params[2].tmpref.size = SharedMem2.size;
+
+	TeecOperation.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT,
+						    TEEC_MEMREF_TEMP_INPUT,
+						    TEEC_MEMREF_TEMP_INOUT,
+						    TEEC_NONE);
+
+	TeecResult = TEEC_InvokeCommand(&TeecSession,
+					122,
+					&TeecOperation,
+					&ErrorOrigin);
+	if (TeecResult != TEEC_SUCCESS) {
+		printf("no keybox in secure storage, write keybox to secure storage\n");
+		TeecResult = TEEC_InvokeCommand(&TeecSession,
+						121,
+						&TeecOperation,
+						&ErrorOrigin);
+		if (TeecResult != TEEC_SUCCESS)
+			printf("send data to TA failed with code 0x%x\n", TeecResult);
+		else
+			printf("send data to TA success with code 0x%x\n", TeecResult);
+	}
+
+	TEEC_ReleaseSharedMemory(&SharedMem0);
+	TEEC_ReleaseSharedMemory(&SharedMem1);
+	TEEC_ReleaseSharedMemory(&SharedMem2);
+
+	TEEC_CloseSession(&TeecSession);
+	TEEC_FinalizeContext(&TeecContext);
+
+	return TeecResult;
+}
+
+int write_keybox_to_secure_storage(uint8_t *uboot_data, uint32_t len)
+{
+	uint32_t key_size;
+	uint32_t data_size;
+	uint32_t object_id;
+	TEEC_Result ret;
+
+	if (memcmp(uboot_data, WIDEVINE_TAG, 4) == 0) {
+		/* widevine keybox */
+		TEEC_UUID widevine_uuid = { 0xc11fe8ac, 0xb997, 0x48cf,
+			{ 0xa2, 0x8d, 0xe2, 0xa5, 0x5e, 0x52, 0x40, 0xef} };
+		object_id = 101;
+
+		key_size = *(uboot_data + 4);
+		data_size = *(uboot_data + 8);
+
+		ret = rk_send_keybox_to_ta((uint8_t *)&object_id,
+					   sizeof(uint32_t),
+					   widevine_uuid,
+					   uboot_data + 12,
+					   key_size,
+					   uboot_data + 12 + key_size,
+					   data_size);
+
+		if (ret == TEEC_SUCCESS)
+			printf("write widevine keybox success\n");
+		else
+			printf("write widevine keybox fail\n");
+	} else if (memcmp(uboot_data, ATTESTATION_TAG, 4) == 0) {
+		/* attestation key */
+	}
+
+	return ret;
+}
 
 void test_optee(void)
 {
