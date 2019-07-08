d3bec1ae5be320cc927a72d0caf4ab66971ac69d
Paul Emge <paulemge@forallsecure.com>
2019-07-08 16:37:05 -0700

    UPSTREAM: CVE-2019-13104: ext4: check for underflow in ext4fs_read_file
    in ext4fs_read_file, it is possible for a broken/malicious file
    system to cause a memcpy of a negative number of bytes, which
    overflows all memory. This patch fixes the issue by checking for
    a negative length.
    
    Signed-off-by: Paul Emge <paulemge@forallsecure.com>
    (cherry picked from commit 878269dbe74229005dd7f27aca66c554e31dad8e)
    Change-Id: I818fc87aa471c348e3cdae0495b5ecf63462b94b

--- fs/ext4/ext4fs.c
+++ fs/ext4/ext4fs.c
@@ -64,12 +64,14 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 	char *delayed_buf = NULL;
 	short status;
 
-	if (blocksize <= 0)
-		return -1;
-
 	/* Adjust len so it we can't read past the end of the file. */
 	if (len + pos > filesize)
 		len = (filesize - pos);
+
+	if (blocksize <= 0 || len <= 0) {
+		ext_cache_fini(&cache);
+		return -1;
+	}
 
 	blockcnt = lldiv(((len + pos) + blocksize - 1), blocksize);
 
