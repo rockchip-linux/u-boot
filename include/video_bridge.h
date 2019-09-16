888cd311e3bcdaff1c7f3e7c03f2c87b3f8bcd41
Algea Cao <algea.cao@rock-chips.com>
2019-09-16 10:28:58 +0800

    video/drm: Add bridge get_timing interface
    Some bridge can't get tv edid such as rk1000 cvbs, but
    at present bridge can't provide timing without edid. So
    we add get_timing interface to provide timing.
    
    Change-Id: Id74802a3bff8140e3677f6633788aacd021afc91
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>

--- include/video_bridge.h
+++ include/video_bridge.h
@@ -62,6 +62,14 @@ struct video_bridge_ops {
 	 * @return number of bytes read, <=0 for error
 	 */
 	int (*read_edid)(struct udevice *dev, u8 *buf, int buf_size);
+
+	/**
+	 * get_timing() - Get timing from bridge
+	 *
+	 * @dev:	Device to get timing
+	 * @return 0 if OK, -ve on error
+	 */
+	int (*get_timing)(struct udevice *dev);
 };
 
 #define video_bridge_get_ops(dev) \
@@ -108,4 +116,11 @@ int video_bridge_check_attached(struct udevice *dev);
  */
 int video_bridge_read_edid(struct udevice *dev, u8 *buf, int buf_size);
 
+/**
+ * video_bridge_get_timing() - Get timing from bridge
+ *
+ * @dev:	Device to get timing
+ * @return 0 if OK, -ve on error
+ */
+int video_bridge_get_timing(struct udevice *dev);
 #endif
