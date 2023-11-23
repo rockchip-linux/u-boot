f252ff79a6581b5b1ec5bb39aa4d2edb46195d1f
Lin Jinhan <troy.lin@rock-chips.com>
2023-11-23 19:56:09 +0800

    crypto: mod_exp_sw: add register algorithms to crypto manager
    Signed-off-by: Lin Jinhan <troy.lin@rock-chips.com>
    Change-Id: I028ef72396b7dc8f52fa41f279f25cd65e65a212

--- drivers/crypto/rsa_mod_exp/mod_exp_sw.c
+++ drivers/crypto/rsa_mod_exp/mod_exp_sw.c
@@ -5,6 +5,7 @@
  */
 
 #include <config.h>
+#include <crypto_manager.h>
 #include <dm.h>
 #include <log.h>
 #include <u-boot/rsa-mod-exp.h>
@@ -23,6 +24,40 @@ static int mod_exp_sw(struct udevice *dev, const uint8_t *sig, uint32_t sig_len,
 	return 0;
 }
 
+#if CONFIG_IS_ENABLED(CRYPTO_MANAGER)
+static bool sw_mod_exp_check_valid(struct udevice *dev, u32 algo, u32 mode)
+{
+	if (mode != CRYPTO_MODE_NONE)
+		return false;
+
+	if (algo != ASYM_ALGO_RSA)
+		return false;
+
+	return true;
+}
+
+static struct crypto_impl sw_mod_exp_impl = {
+	.type        = CRYPTO_TYPE_ASYM,
+	.uclass_id   = UCLASS_MOD_EXP,
+	.priority    = CRYPTO_PRIORITY_SW,
+	.check_valid = sw_mod_exp_check_valid,
+
+	.asym.rsa.mod_exp = mod_exp_sw,
+};
+
+static int sw_mod_exp_bind(struct udevice *dev)
+{
+	sw_mod_exp_impl.dev = dev;
+
+	return crypto_impl_register(&sw_mod_exp_impl);
+}
+#else
+static int sw_mod_exp_bind(struct udevice *dev)
+{
+	return 0;
+}
+#endif
+
 static const struct mod_exp_ops mod_exp_ops_sw = {
 	.mod_exp	= mod_exp_sw,
 };
@@ -31,6 +66,7 @@ U_BOOT_DRIVER(mod_exp_sw) = {
 	.name	= "mod_exp_sw",
 	.id	= UCLASS_MOD_EXP,
 	.ops	= &mod_exp_ops_sw,
+	.bind   = sw_mod_exp_bind,
 	.flags	= DM_FLAG_PRE_RELOC,
 };
 
