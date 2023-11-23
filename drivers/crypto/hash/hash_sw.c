579dd12ab1ae3e050af674141bba7127cba07a63
Lin Jinhan <troy.lin@rock-chips.com>
2023-11-23 16:52:01 +0800

    crypto: hash_sw: register algorithms to crypto manager
    Signed-off-by: Lin Jinhan <troy.lin@rock-chips.com>
    Change-Id: Iaba2e1527b1352a929c4fd8d129f64717be5ec84

--- drivers/crypto/hash/hash_sw.c
+++ drivers/crypto/hash/hash_sw.c
@@ -4,6 +4,7 @@
  * Author: ChiaWei Wang <chiawei_wang@aspeedtech.com>
  */
 #include <config.h>
+#include <crypto_manager.h>
 #include <dm.h>
 #include <log.h>
 #include <malloc.h>
@@ -32,6 +33,7 @@ static void hash_finish_crc16_ccitt(void *ctx, void *obuf)
 }
 
 /* CRC32 */
+#ifdef CONFIG_CRC32
 static void hash_init_crc32(void *ctx)
 {
 	*((uint32_t *)ctx) = 0;
@@ -46,8 +48,10 @@ static void hash_finish_crc32(void *ctx, void *obuf)
 {
 	*((uint32_t *)obuf) = *((uint32_t *)ctx);
 }
+#endif
 
 /* MD5 */
+#ifdef CONFIG_MD5
 static void hash_init_md5(void *ctx)
 {
 	MD5Init((MD5Context *)ctx);
@@ -62,8 +66,10 @@ static void hash_finish_md5(void *ctx, void *obuf)
 {
 	MD5Final(obuf, (MD5Context *)ctx);
 }
+#endif
 
 /* SHA1 */
+#ifdef CONFIG_SHA1
 static void hash_init_sha1(void *ctx)
 {
 	sha1_starts((sha1_context *)ctx);
@@ -78,8 +84,10 @@ static void hash_finish_sha1(void *ctx, void *obuf)
 {
 	sha1_finish((sha1_context *)ctx, obuf);
 }
+#endif
 
 /* SHA256 */
+#ifdef CONFIG_SHA256
 static void hash_init_sha256(void *ctx)
 {
 	sha256_starts((sha256_context *)ctx);
@@ -94,8 +102,10 @@ static void hash_finish_sha256(void *ctx, void *obuf)
 {
 	sha256_finish((sha256_context *)ctx, obuf);
 }
+#endif
 
 /* SHA384 */
+#ifdef CONFIG_SHA384
 static void hash_init_sha384(void *ctx)
 {
 	sha384_starts((sha512_context *)ctx);
@@ -110,8 +120,10 @@ static void hash_finish_sha384(void *ctx, void *obuf)
 {
 	sha384_finish((sha512_context *)ctx, obuf);
 }
+#endif
 
 /* SHA512 */
+#ifdef CONFIG_SHA512
 static void hash_init_sha512(void *ctx)
 {
 	sha512_starts((sha512_context *)ctx);
@@ -126,6 +138,7 @@ static void hash_finish_sha512(void *ctx, void *obuf)
 {
 	sha512_finish((sha512_context *)ctx, obuf);
 }
+#endif
 
 struct sw_hash_ctx {
 	enum HASH_ALGO algo;
@@ -146,48 +159,54 @@ static struct sw_hash_impl sw_hash_impl[HASH_ALGO_NUM] = {
 		.finish = hash_finish_crc16_ccitt,
 		.ctx_alloc_sz = sizeof(uint16_t),
 	},
-
+#ifdef CONFIG_CRC32
 	[HASH_ALGO_CRC32] = {
 		.init = hash_init_crc32,
 		.update = hash_update_crc32,
 		.finish = hash_finish_crc32,
 		.ctx_alloc_sz = sizeof(uint32_t),
 	},
-
+#endif
+#ifdef CONFIG_MD5
 	[HASH_ALGO_MD5] = {
 		.init = hash_init_md5,
 		.update = hash_update_md5,
 		.finish = hash_finish_md5,
 		.ctx_alloc_sz = sizeof(MD5Context),
 	},
-
+#endif
+#ifdef CONFIG_SHA1
 	[HASH_ALGO_SHA1] = {
 		.init = hash_init_sha1,
 		.update = hash_update_sha1,
 		.finish = hash_finish_sha1,
 		.ctx_alloc_sz = sizeof(sha1_context),
 	},
-
+#endif
+#ifdef CONFIG_SHA256
 	[HASH_ALGO_SHA256] = {
 		.init = hash_init_sha256,
 		.update = hash_update_sha256,
 		.finish = hash_finish_sha256,
 		.ctx_alloc_sz = sizeof(sha256_context),
 	},
-
+#endif
+#ifdef CONFIG_SHA384
 	[HASH_ALGO_SHA384] = {
 		.init = hash_init_sha384,
 		.update = hash_update_sha384,
 		.finish = hash_finish_sha384,
 		.ctx_alloc_sz = sizeof(sha512_context),
 	},
-
+#endif
+#ifdef CONFIG_SHA512
 	[HASH_ALGO_SHA512] = {
 		.init = hash_init_sha512,
 		.update = hash_update_sha512,
 		.finish = hash_finish_sha512,
 		.ctx_alloc_sz = sizeof(sha512_context),
 	},
+#endif
 };
 
 static int sw_hash_init(struct udevice *dev, enum HASH_ALGO algo, void **ctxp)
@@ -288,10 +307,72 @@ static const struct hash_ops hash_ops_sw = {
 	.hash_digest = sw_hash_digest,
 };
 
+#if CONFIG_IS_ENABLED(CRYPTO_MANAGER)
+static bool sw_hash_check_valid(struct udevice *dev, u32 algo, u32 mode)
+{
+	if (mode != CRYPTO_MODE_NONE)
+		return false;
+
+	if (algo == HASH_ALGO_CRC16_CCITT)
+		return true;
+	else if ((algo == HASH_ALGO_CRC32) && IS_ENABLED(CONFIG_CRC32))
+		return true;
+	else if ((algo == HASH_ALGO_MD5) && IS_ENABLED(CONFIG_MD5))
+		return true;
+	else if ((algo == HASH_ALGO_SHA1) && IS_ENABLED(CONFIG_SHA1))
+		return true;
+	else if ((algo == HASH_ALGO_SHA256) && IS_ENABLED(CONFIG_SHA256))
+		return true;
+	else if ((algo == HASH_ALGO_SHA384) && IS_ENABLED(CONFIG_SHA384))
+		return true;
+	else if ((algo == HASH_ALGO_SHA512) && IS_ENABLED(CONFIG_SHA512))
+		return true;
+
+	return false;
+}
+
+static u32 sw_hash_dynamic_priority(struct udevice *dev, u32 algo, u32 mode)
+{
+	u32 prio;
+
+	if ((algo == HASH_ALGO_SHA1) && IS_ENABLED(CONFIG_ARMV8_CE_SHA1))
+		prio = CRYPTO_PRIORITY_BEST;
+	else if ((algo == HASH_ALGO_SHA256) && IS_ENABLED(CONFIG_ARMV8_CE_SHA256))
+		prio = CRYPTO_PRIORITY_BEST;
+	else
+		prio = CRYPTO_PRIORITY_SW;
+
+	return prio;
+}
+
+static struct crypto_impl sw_crypto_hash_impl = {
+	.type              = CRYPTO_TYPE_HASH,
+	.uclass_id         = UCLASS_HASH,
+	.dynamic_priority  = sw_hash_dynamic_priority,
+	.check_valid       = sw_hash_check_valid,
+	.hash.hash_init    = sw_hash_init,
+	.hash.hash_update  = sw_hash_update,
+	.hash.hash_finish  = sw_hash_finish,
+};
+
+static int sw_hash_bind(struct udevice *dev)
+{
+	sw_crypto_hash_impl.dev = dev;
+
+	return crypto_impl_register(&sw_crypto_hash_impl);
+}
+#else
+static int sw_hash_bind(struct udevice *dev)
+{
+	return 0;
+}
+#endif
+
 U_BOOT_DRIVER(hash_sw) = {
 	.name = "hash_sw",
 	.id = UCLASS_HASH,
 	.ops = &hash_ops_sw,
+	.bind = sw_hash_bind,
 	.flags = DM_FLAG_PRE_RELOC,
 };
 
