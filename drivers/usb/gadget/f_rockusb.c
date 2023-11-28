1dd9b2ce46a6b56f2551136fc8bf7c9dac4af6eb
Hisping Lin <hisping.lin@rock-chips.com>
2023-11-28 15:28:58 +0800

    rockusb: support write oem otp key
    Change-Id: I0ef3a46e70c9b6cf6fe2f91911ca3ea6808379a8
    Signed-off-by: Hisping Lin <hisping.lin@rock-chips.com>

--- drivers/usb/gadget/f_rockusb.c
+++ drivers/usb/gadget/f_rockusb.c
@@ -580,6 +580,19 @@ static int rkusb_do_vs_write(struct fsg_common *common)
 						curlun->sense_data = SS_WRITE_ERROR;
 						return -EIO;
 					}
+				} else if (memcmp(data, "OTPK", 4) == 0) {
+					uint32_t key_len = vhead->size - 9;
+					uint8_t key_id = *((uint8_t *)data + 8);
+					if (key_len != 16 && key_len != 24 && key_len != 32) {
+						printf("check oem otp key size fail!\n");
+						curlun->sense_data = SS_WRITE_ERROR;
+						return -EIO;
+					}
+					if (trusty_write_oem_otp_key(key_id, (uint8_t *)(data + 9), key_len) != 0) {
+						printf("trusty_write_oem_huk error!");
+						curlun->sense_data = SS_WRITE_ERROR;
+						return -EIO;
+					}
 				} else {
 					printf("Unknown tag\n");
 					curlun->sense_data = SS_WRITE_ERROR;
