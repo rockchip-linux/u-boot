511fd8445786c1a8a99468542c6dd3818d5c18d0
Joseph Chen <chenjh@rock-chips.com>
2023-12-07 06:42:14 +0000

    cpu: rockchip amp: Allow override kernel address from amp.img
    - define "load = <...>;" to override "kernel_addr_r"
    - define "load_c = <...>;" to override "kernel_addr_c"
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I0827758dc236daa7cf5e7d56ca6c99a32f35df90
    (cherry picked from commit 1dc5141f2c2bbbaff1a2e6205aa1200c360f0e05)

--- drivers/cpu/rockchip_amp.c
+++ drivers/cpu/rockchip_amp.c
@@ -254,7 +254,7 @@ static int brought_up_amp(void *fit, int noffset,
 	const char *desc;
 	boot_args_t args;
 	u32 cpu, aarch64, hyp;
-	u32 load, thumb, us;
+	u32 load, load_c, thumb, us;
 	u32 pe_state, entry, pass_fdt;
 	int boot_on;
 	int data_size;
@@ -267,6 +267,7 @@ static int brought_up_amp(void *fit, int noffset,
 	hyp = fit_get_u32_default(fit, noffset, "hyp", 0);
 	thumb = fit_get_u32_default(fit, noffset, "thumb", 0);
 	load = fit_get_u32_default(fit, noffset, "load", -ENODATA);
+	load_c = fit_get_u32_default(fit, noffset, "load_c", -ENODATA);
 	pass_fdt = fit_get_u32_default(fit, noffset, "pass-fdt", 0);
 	entry = fit_get_u32_default(fit, noffset, "entry", -ENODATA);
 	us = fit_get_u32_default(fit, noffset, "udelay", 0);
@@ -294,6 +295,14 @@ static int brought_up_amp(void *fit, int noffset,
 		AMP_E("Property missing!\n");
 		return -EINVAL;
 	}
+
+	if (is_linux) {
+		if (load != -ENODATA)
+			env_set_hex("kernel_addr_r", load);
+		if (load_c != -ENODATA)
+			env_set_hex("kernel_addr_c", load_c);
+	}
+
 	aarch64 = (arch == IH_ARCH_ARM) ? 0 : 1;
 	pe_state = PE_STATE(aarch64, hyp, thumb, 0);
 
@@ -304,6 +313,7 @@ static int brought_up_amp(void *fit, int noffset,
 	AMP_I("        hyp: %d\n", hyp);
 	AMP_I("      thumb: %d\n", thumb);
 	AMP_I("       load: 0x%08x\n", load);
+	AMP_I("     load_c: 0x%08x\n", load_c);
 	AMP_I("      entry: 0x%08x\n", entry);
 	AMP_I("   pe_state: 0x%08x\n", pe_state);
 	AMP_I("   linux-os: %d\n\n", is_linux);
@@ -537,4 +547,3 @@ int arm64_switch_amp_pe(struct bootm_headers *images)
 	images->os.arch = g_bootcpu.arch;
 	return g_bootcpu.state;
 }
-
