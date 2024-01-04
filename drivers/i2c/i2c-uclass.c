b42ddf56a5efcb317373b0ad1c6099f1ea9e31d9
Joseph Chen <chenjh@rock-chips.com>
2024-01-04 10:27:19 +0000

    dm: i2c: Add i2c bus info verbose
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I805134a321d7059ab84dc9138bbdc86b273c7b76

--- drivers/i2c/i2c-uclass.c
+++ drivers/i2c/i2c-uclass.c
@@ -748,9 +748,15 @@ static int i2c_post_probe(struct udevice *dev)
 {
 #if CONFIG_IS_ENABLED(OF_REAL)
 	struct dm_i2c_bus *i2c = dev_get_uclass_priv(dev);
+	int seq;
 
 	i2c->speed_hz = dev_read_u32_default(dev, "clock-frequency",
 					     I2C_SPEED_STANDARD_RATE);
+
+	if (dev_read_alias_seq(dev, &seq) >= 0)
+		printf("I2c%d speed: %dHz\n", seq, i2c->speed_hz);
+	else
+		printf("I2c speed: %dHz\n", i2c->speed_hz);
 
 	return dm_i2c_set_bus_speed(dev, i2c->speed_hz);
 #else
