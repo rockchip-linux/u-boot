1a219a1ac98e2bec87957b6fc382676b00c842d4
Damon Ding <damon.ding@rock-chips.com>
2024-07-05 14:42:20 +0800

    video/drm: sii902x: enable CLK_RATIO_2X for interlace modes
    According to the datasheet, CLK_RATIO_2X should be enabled
    for interlace modes otherwise the 720x480i60/720x576i60
    modes may be mistaken for 360x480p60/360x576p60.
    
    Change-Id: Icb476cb86f93ba0bb009683753ad7f883c7e9dd7
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>
    (cherry picked from commit 404ebce3b3c5c40bf60b5405217bf5e45bdcd0d3)

--- drivers/video/drm/sii902x.c
+++ drivers/video/drm/sii902x.c
@@ -25,6 +25,7 @@
 #define SII902X_TPI_AVI_PIXEL_REP_4X		3
 #define SII902X_TPI_AVI_PIXEL_REP_2X		1
 #define SII902X_TPI_AVI_PIXEL_REP_NONE		0
+#define SII902X_TPI_CLK_RATIO_MASK		GENMASK(7, 6)
 #define SII902X_TPI_CLK_RATIO_HALF		(0 << 6)
 #define SII902X_TPI_CLK_RATIO_1X		(1 << 6)
 #define SII902X_TPI_CLK_RATIO_2X		(2 << 6)
@@ -343,6 +344,7 @@ static void sii902x_bridge_mode_set(struct rockchip_bridge *bridge,
 	u8 buf[HDMI_INFOFRAME_SIZE(AVI)];
 	u8 output_mode;
 	u16 pixel_clock_10kHz = mode->clock / 10;
+	u8 ratio;
 	int ret, vrefresh;
 
 	if (drm_detect_hdmi_monitor(edid))
@@ -364,8 +366,7 @@ static void sii902x_bridge_mode_set(struct rockchip_bridge *bridge,
 	buf[5] = mode->crtc_htotal >> 8;
 	buf[6] = mode->crtc_vtotal;
 	buf[7] = mode->crtc_vtotal >> 8;
-	buf[8] = SII902X_TPI_CLK_RATIO_1X | SII902X_TPI_AVI_PIXEL_REP_NONE |
-		 SII902X_TPI_AVI_PIXEL_REP_BUS_24BIT;
+	buf[8] = SII902X_TPI_AVI_PIXEL_REP_NONE | SII902X_TPI_AVI_PIXEL_REP_BUS_24BIT;
 	switch (sii902x->bus_format) {
 	case MEDIA_BUS_FMT_YUYV8_1X16:
 	case MEDIA_BUS_FMT_YVYU8_1X16:
@@ -408,6 +409,13 @@ static void sii902x_bridge_mode_set(struct rockchip_bridge *bridge,
 		dev_err(dev, "failed to set video data\n");
 		return;
 	}
+
+	if (sii902x->mode.flags & DRM_MODE_FLAG_INTERLACE)
+		ratio = SII902X_TPI_CLK_RATIO_2X;
+	else
+		ratio = SII902X_TPI_CLK_RATIO_1X;
+	sii902x_reg_update_bits(sii902x, SII902X_TPI_PIXEL_REPETITION,
+				SII902X_TPI_CLK_RATIO_MASK, ratio);
 
 	ret = drm_hdmi_avi_infoframe_from_display_mode(&frame, &sii902x->mode, false);
 	if (ret < 0) {
