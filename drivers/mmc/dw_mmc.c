6ec3e64d4dfbdebf35df92da031d1362fbf942d3
Yifeng Zhao <yifeng.zhao@rock-chips.com>
2024-08-01 15:50:45 +0800

    dw_mmc: send the initialization clock with cmd0
    Two TF cards fail to initialize under the controller configuration
    CLKEN_LOW_PWR. It is necessary to add clock initialization when
    sending the first command to resolve this issue.
    
    Signed-off-by: Yifeng Zhao <yifeng.zhao@rock-chips.com>
    Change-Id: I4d44bffd24d14131a61e2332fd555a3d6f92ed0c
    (cherry picked from commit c27bf91c6df11675dcefaf6a1e60b1ef0e120af6)

--- drivers/mmc/dw_mmc.c
+++ drivers/mmc/dw_mmc.c
@@ -407,6 +407,8 @@ static int dwmci_send_cmd_common(struct dwmci_host *host, struct mmc_cmd *cmd,
 
 	if (cmd->cmdidx == MMC_CMD_STOP_TRANSMISSION)
 		flags |= DWMCI_CMD_ABORT_STOP;
+	else if (cmd->cmdidx == MMC_CMD_GO_IDLE_STATE)
+		flags |= SDMMC_CMD_INIT | DWMCI_CMD_ABORT_STOP;
 	else
 		flags |= DWMCI_CMD_PRV_DAT_WAIT;
 
