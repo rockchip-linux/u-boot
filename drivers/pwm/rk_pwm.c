acaa5eb26d803f2486b4e8214bc60bc224b631c0
Damon Ding <damon.ding@rock-chips.com>
2025-02-28 11:06:41 +0800

    pwm: rockchip: fix the polarity configurations for pwm v4
    Fixes: 6a624b88fcd ("pwm: rockchip: add support for rk3576")
    Change-Id: I9b8df3609f12f8cce1c5a3ce66e97cf0e120351e
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/pwm/rk_pwm.c
+++ drivers/pwm/rk_pwm.c
@@ -95,9 +95,9 @@ static int rk_pwm_set_invert_v4(struct udevice *dev, uint channel, bool polarity
 
 	debug("%s: polarity=%u\n", __func__, polarity);
 	if (polarity)
-		priv->conf_polarity = PWM_DUTY_NEGATIVE | PWM_INACTIVE_POSTIVE;
+		priv->conf_polarity = DUTY_NEGATIVE | INACTIVE_POSITIVE;
 	else
-		priv->conf_polarity = PWM_DUTY_POSTIVE | PWM_INACTIVE_NEGATIVE;
+		priv->conf_polarity = DUTY_POSITIVE | INACTIVE_NEGATIVE;
 
 	return 0;
 }
@@ -292,8 +292,13 @@ static int rk_pwm_probe(struct udevice *dev)
 	priv->freq = ret;
 	priv->data = (struct rockchip_pwm_data *)dev_get_driver_data(dev);
 
-	if (priv->data->supports_polarity)
-		priv->conf_polarity = PWM_DUTY_POSTIVE | PWM_INACTIVE_NEGATIVE;
+	if (priv->data->supports_polarity) {
+		if (priv->data->main_version >= 4) {
+			priv->conf_polarity = DUTY_POSITIVE | INACTIVE_NEGATIVE;
+		} else {
+			priv->conf_polarity = PWM_DUTY_POSTIVE | PWM_INACTIVE_POSTIVE;
+		}
+	}
 
 	return 0;
 }
