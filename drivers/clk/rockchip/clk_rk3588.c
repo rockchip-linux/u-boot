301db610f8aafef35b03b2a2064e063a576f49a7
Elaine Zhang <zhangqing@rock-chips.com>
2025-06-18 11:03:51 +0800

    clk: rockchip: rk3588: sync code
    Sync from next-dev:
    feb2c9de35d4 (rockchip: rv1106: Add CONFIG_SPL_CHECK_SIZE)
    
    Change-Id: I6c8f106fb11d15d65579d838daa6381ba441c4de
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- drivers/clk/rockchip/clk_rk3588.c
+++ drivers/clk/rockchip/clk_rk3588.c
@@ -28,6 +28,7 @@ static struct rockchip_pll_rate_table rk3588_pll_rates[] = {
 	RK3588_PLL_RATE(1500000000, 2, 250, 1, 0),
 	RK3588_PLL_RATE(1200000000, 2, 200, 1, 0),
 	RK3588_PLL_RATE(1188000000, 2, 198, 1, 0),
+	RK3588_PLL_RATE(1150000000, 3, 575, 2, 0),
 	RK3588_PLL_RATE(1100000000, 3, 550, 2, 0),
 	RK3588_PLL_RATE(1008000000, 2, 336, 2, 0),
 	RK3588_PLL_RATE(1000000000, 3, 500, 2, 0),
@@ -1168,6 +1169,8 @@ static ulong rk3588_dclk_vop_set_clk(struct rk3588_clk_priv *priv,
 				     ((div - 1) << div_shift));
 		} else {
 			div = DIV_ROUND_UP(RK3588_VOP_PLL_LIMIT_FREQ, rate);
+			if (div % 2)
+				div = div + 1;
 			rk_clrsetreg(&cru->clksel_con[conid],
 				     mask,
 				     DCLK_VOP_SRC_SEL_V0PLL << sel_shift |
@@ -1219,6 +1222,37 @@ static ulong rk3588_dclk_vop_set_clk(struct rk3588_clk_priv *priv,
 		}
 	}
 	return rk3588_dclk_vop_get_clk(priv, clk_id);
+}
+
+static ulong rk3588_clk_csihost_get_clk(struct rk3588_clk_priv *priv, ulong clk_id)
+{
+	struct rk3588_cru *cru = priv->cru;
+	u32 div, sel, con, parent;
+
+	switch (clk_id) {
+	case CLK_DSIHOST0:
+		con = readl(&cru->clksel_con[114]);
+		break;
+	case CLK_DSIHOST1:
+		con = readl(&cru->clksel_con[115]);
+		break;
+	default:
+		return -ENOENT;
+	}
+
+	div = (con & CLK_DSIHOST_DIV_MASK) >> CLK_DSIHOST_DIV_SHIFT;
+	sel = (con & CLK_DSIHOST_SEL_MASK) >> CLK_DSIHOST_SEL_SHIFT;
+
+	if (sel == CLK_DSIHOST_SEL_GPLL)
+		parent = priv->gpll_hz;
+	else if (sel == CLK_DSIHOST_SEL_CPLL)
+		parent = priv->cpll_hz;
+	else if (sel == CLK_DSIHOST_SEL_V0PLL)
+		parent = priv->v0pll_hz;
+	else
+		parent = priv->spll_hz;
+
+	return DIV_TO_RATE(parent, div);
 }
 
 static ulong rk3588_gmac_get_clk(struct rk3588_clk_priv *priv, ulong clk_id)
@@ -1637,6 +1671,10 @@ static ulong rk3588_clk_get_rate(struct clk *clk)
 	case DCLK_VOP3:
 		rate = rk3588_dclk_vop_get_clk(priv, clk->id);
 		break;
+	case CLK_DSIHOST0:
+	case CLK_DSIHOST1:
+		rate = rk3588_clk_csihost_get_clk(priv, clk->id);
+		break;
 	case CLK_GMAC0_PTP_REF:
 	case CLK_GMAC1_PTP_REF:
 	case CLK_GMAC_125M:
@@ -2051,6 +2089,7 @@ static void rk3588_clk_init(struct rk3588_clk_priv *priv)
 		     ACLK_BUS_ROOT_DIV_MASK,
 		     div << ACLK_BUS_ROOT_DIV_SHIFT);
 
+	priv->spll_hz = 702000000;
 	if (priv->cpll_hz != CPLL_HZ) {
 		ret = rockchip_pll_set_rate(&rk3588_pll_clks[CPLL], priv->cru,
 					    CPLL, CPLL_HZ);
