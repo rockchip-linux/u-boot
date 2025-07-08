095c23a660e44b56cbba46cd0de6bedaec8fd185
Xuhui Lin <xuhui.lin@rock-chips.com>
2025-07-08 10:39:51 +0800

    rockchip: param: Find out the last mem size that is not 0
    If base + size > SZ_4G, param_parse_ddr_mem() will use 'base_u64'
    and 'size_u64' to record mem above 4G, and base and size will be
    zero for this bank. So keep detect until mem_size is not zero.
    
    Change-Id: If32ea524384a39e6c748d34a013edd52199de9a1
    Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>
    (cherry picked from commit d2bd2a679f2cf6bc9b6d76fef08d127e584f8f18)

--- arch/arm/mach-rockchip/param.c
+++ arch/arm/mach-rockchip/param.c
@@ -398,6 +398,7 @@ struct memblock *param_parse_ddr_mem(int *out_count)
 phys_size_t param_simple_parse_ddr_mem(int init_bank)
 {
 	struct memblock *list;
+	phys_size_t mem_size;
 	int i, count;
 
 	list = param_parse_ddr_mem(&count);
@@ -412,8 +413,16 @@ phys_size_t param_simple_parse_ddr_mem(int init_bank)
 	}
 
 	if (!init_bank) {
-		i = count - 1;
-		return ddr_mem_get_usable_size(list[i].base, list[i].size);
+		/*
+		 * If base + size > SZ_4G, param_parse_ddr_mem() will use 'base_u64'
+		 * and 'size_u64' to record mem above 4G, and base and size will be
+		 * zero for this bank. So keep detect until mem_size is not zero.
+		 */
+		for (i = count - 1; i >= 0; i--) {
+			mem_size = ddr_mem_get_usable_size(list[i].base, list[i].size);
+			if (mem_size)
+				return mem_size;
+		}
 	}
 
 	for (i = 0; i < count; i++) {
