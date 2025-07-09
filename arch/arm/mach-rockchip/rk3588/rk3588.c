0de446c264e81c6a96db49357c76844d0f6ec714
YouMin Chen <cym@rock-chips.com>
2025-07-09 18:30:16 +0800

    rockchip: rk3588: Sync mmu map regions and dram_init_banksize
    Sync from next-dev:
    18b12094ee4 video/drm: dsi2: fix error that cri fifo is not empty after restarting dsi host
    
    Change-Id: I3dbb59e91311c6db280e79834dc1b4f1a3aa8189
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Signed-off-by: YouMin Chen <cym@rock-chips.com>

--- arch/arm/mach-rockchip/rk3588/rk3588.c
+++ arch/arm/mach-rockchip/rk3588/rk3588.c
@@ -129,6 +129,12 @@ static struct mm_region rk3588_mem_map[] = {
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
 	},  {
+		.virt = 0x100000000UL,
+		.phys = 0x100000000UL,
+		.size = 0x700000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
 		.virt = 0x900000000,
 		.phys = 0x900000000,
 		.size = 0x150000000,
