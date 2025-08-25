e07a1f3080b2f79d2551441ac6eb67601a2f3bdd
Damon Ding <damon.ding@rock-chips.com>
2025-08-25 15:58:14 +0800

    video/drm: display: Check &display_state.is_init before display_bmp()
    The commit to be fixed is not well thought out. It just take into
    account the scenario where the U-Boot logo is already displayed and
    the specific BMP is showed via the cmd 'rockchip_show_bmp'.
    
    For the charge animation driver, the rockchip_show_bmp() will be
    called without rockchip_show_logo() invoked, which means the
    &display_state.is_init is false. It makes the charge BMPs unable to
    be displayed as expected.
    
    Therefore, we add the &display_state.is_init check before
    display_bmp() in API rockchip_show_bmp(). It helps confirm that the
    API rockchip_show_bmp() can work well as before.
    
    Fixes: fc5f6aa8274 ("video/drm: display: Use new display_bmp() instead of display_logo for command rockchip_show_bmp")
    Change-Id: I66688c0620443c086222d73fbb9d8602d601af30
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/video/drm/rockchip_display.c
+++ drivers/video/drm/rockchip_display.c
@@ -1137,9 +1137,6 @@ static int display_bmp(struct display_state *state)
 	u32 overscan_w, overscan_h;
 	int hdisplay, vdisplay, ret;
 
-	if (!state->is_init)
-		return -ENODEV;
-
 	switch (logo->bpp) {
 	case 16:
 		crtc_state->format = ROCKCHIP_FMT_RGB565;
@@ -1755,7 +1752,10 @@ int rockchip_show_bmp(const char *bmp)
 		s->logo.mode = s->charge_logo_mode;
 		if (load_bmp_logo(&s->logo, bmp))
 			continue;
-		ret = display_bmp(s);
+		if (!s->is_init)
+			ret = display_logo(s);
+		else
+			ret = display_bmp(s);
 	}
 
 	return ret;
