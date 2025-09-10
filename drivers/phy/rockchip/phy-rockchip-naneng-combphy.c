1101f09de3e94c362a2db2ca603e4182317306a2
William Wu <william.wu@rock-chips.com>
2025-09-10 17:15:51 +0800

    phy: rockchip: naneng-combphy: Fix usb3 phy init for rockusb2.0
    For some rk3528/rk3562/rk3568 platforms which only
    support usb2.0 rockusb download, they can enable the
    'CONFIG_ROCKUSB_MAX_SPEED_HS' to avoid initializing
    the usbdp phy and force the rockusb download mode to
    work at high speed.
    
    Change-Id: Id068b41478bcfd914314f25a0b5649fceba0ca39
    Signed-off-by: William Wu <william.wu@rock-chips.com>

--- drivers/phy/rockchip/phy-rockchip-naneng-combphy.c
+++ drivers/phy/rockchip/phy-rockchip-naneng-combphy.c
@@ -101,6 +101,7 @@ static int param_write(struct regmap *base,
 	return regmap_write(base, reg->offset, val);
 }
 
+#if !CONFIG_IS_ENABLED(ROCKUSB_MAX_SPEED_HS)
 static u32 rockchip_combphy_is_ready(struct rockchip_combphy_priv *priv)
 {
 	const struct rockchip_combphy_grfcfg *cfg = priv->cfg->grfcfg;
@@ -114,6 +115,7 @@ static u32 rockchip_combphy_is_ready(struct rockchip_combphy_priv *priv)
 
 	return val;
 }
+#endif
 
 static int rockchip_combphy_pcie_init(struct rockchip_combphy_priv *priv)
 {
@@ -197,6 +199,7 @@ static int rockchip_combphy_sgmii_init(struct rockchip_combphy_priv *priv)
 	return ret;
 }
 
+#if !CONFIG_IS_ENABLED(ROCKUSB_MAX_SPEED_HS)
 int rockchip_combphy_usb3_uboot_init(fdt_addr_t phy_addr)
 {
 	struct udevice *udev = NULL;
@@ -256,6 +259,7 @@ int rockchip_combphy_usb3_uboot_init(fdt_addr_t phy_addr)
 
 	return ret;
 }
+#endif
 
 static int rockchip_combphy_set_mode(struct rockchip_combphy_priv *priv)
 {
--- include/linux/usb/phy-rockchip-naneng-combphy.h
+++ include/linux/usb/phy-rockchip-naneng-combphy.h
@@ -6,7 +6,7 @@
 #ifndef _PHY_ROCKCHIP_NANENG_COMBPHY_H
 #define _PHY_ROCKCHIP_NANENG_COMBPHY_H
 
-#if CONFIG_IS_ENABLED(PHY_ROCKCHIP_NANENG_COMBOPHY)
+#if CONFIG_IS_ENABLED(PHY_ROCKCHIP_NANENG_COMBOPHY) && !CONFIG_IS_ENABLED(ROCKUSB_MAX_SPEED_HS)
 int rockchip_combphy_usb3_uboot_init(fdt_addr_t phy_addr);
 #else
 int rockchip_combphy_usb3_uboot_init(fdt_addr_t phy_addr)
