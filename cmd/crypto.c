80660e33393dc6fc00768e47d9454f81f8e4b9ba
Lin Jinhan <troy.lin@rock-chips.com>
2025-11-04 01:07:08 +0000

    cmd/crypto: add ecdsa verify test
    Signed-off-by: Lin Jinhan <troy.lin@rock-chips.com>
    Change-Id: I747872dd8b31f422569d023b94944109b96ede5e

--- cmd/crypto.c
+++ cmd/crypto.c
@@ -66,6 +66,19 @@ struct rsa_test_data {
 	u32		sig_data_len;
 };
 
+struct ec_test_data {
+	const char	*algo_name;
+	u32		algo;
+	const u8	*pub_x;
+	u32		pub_x_len;
+	const u8	*pub_y;
+	u32		pub_y_len;
+	const u8	*hash_in;
+	u32		hash_in_len;
+	const u8	*sign_in;
+	u32		sign_in_len;
+};
+
 #define IS_MAC_MODE(mode)	((mode) == CIPHER_MODE_CBC_MAC || \
 				 (mode) == CIPHER_MODE_CMAC)
 
@@ -132,6 +145,20 @@ struct rsa_test_data {
 	.sig_data_len  = sizeof(sig), \
 }
 
+
+#define EC_TEST(name, x, y, hash, sign) { \
+	.algo_name   = #name, \
+	.algo        = ASYM_ALGO_ECC, \
+	.pub_x       = (x), \
+	.pub_x_len   = sizeof(x), \
+	.pub_y       = (y), \
+	.pub_y_len   = sizeof(y), \
+	.hash_in     = (hash), \
+	.hash_in_len = sizeof(hash), \
+	.sign_in     = (sign), \
+	.sign_in_len = sizeof(sign), \
+}
+
 #define EMPTY_TEST() {}
 
 const struct hash_test_data hash_data_set[] = {
@@ -194,6 +221,15 @@ const struct cipher_test_data cipher_data_set[] = {
 const struct rsa_test_data rsa_data_set[] = {
 #if CONFIG_IS_ENABLED(ROCKCHIP_RSA)
 	RSA_TEST(2048, rsa2048_key_der, rsa_hash_data, rsa2048_sig_data),
+#endif
+};
+
+const struct ec_test_data ec_data_set[] = {
+#if CONFIG_IS_ENABLED(ROCKCHIP_EC)
+	EC_TEST(secp192r1, ecc192r1_pub_x, ecc192r1_pub_y, ecc192r1_hash, ecc192r1_sign),
+	EC_TEST(sm2p256v1, sm2_pub_x, sm2_pub_y, sm2_hash, sm2_sign),
+#else
+	EMPTY_TEST(),
 #endif
 };
 
@@ -606,6 +642,62 @@ static void dump_crypto_info(const struct crypto_impl *impl)
 	printf("================================================\n\n");
 }
 
+int test_ec_result(void)
+{
+	const struct ec_test_data *test_data = NULL;
+	ulong start, time_cost;
+	struct udevice *dev;
+	const struct ecdsa_ops *ops;
+	struct ecdsa_public_key ec_key;
+	int ret, i;
+
+	printf("\n====================== ec test ========================\n");
+	for (i = 0; i < ARRAY_SIZE(ec_data_set); i++) {
+		test_data = &ec_data_set[i];
+		if (test_data->algo_name == NULL) {
+			printf("\n");
+			continue;
+		}
+
+		ret = uclass_get_device(UCLASS_ECDSA, 0, &dev);
+		if (ret || !dev) {
+			printf("[%s] %-16s unsupported!!!\n",
+			       test_data->algo_name, "");
+			continue;
+		}
+
+		/* verify test */
+		memset(&ec_key, 0x00, sizeof(ec_key));
+		ec_key.curve_name = test_data->algo_name;
+		ec_key.x = test_data->pub_x;
+		ec_key.y = test_data->pub_y;
+		ec_key.size_bits = test_data->pub_x_len * 8;
+
+		start = get_timer(0);
+		ops = dev_get_driver_ops(dev);
+		ret = ops->verify(dev, &ec_key,
+				  (u8 *)test_data->hash_in,
+				  test_data->hash_in_len,
+				  (u8 *)test_data->sign_in,
+				  test_data->sign_in_len);
+		if (ret) {
+			printf("verify test error, ret = %d\n", ret);
+			goto error;
+		}
+		time_cost = get_timer(start);
+
+		printf("[%-9s]   %-8s PASS    (%lums)\n",
+		       test_data->algo_name, "verify", time_cost);
+
+		printf("+++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+	}
+
+	return 0;
+error:
+	printf("%s test error!\n", test_data->algo_name);
+	return ret;
+}
+
 static int do_crypto(struct cmd_tbl *cmdtp, int flag, int argc, char * const argv[])
 {
 	const struct crypto_impl *impl = NULL;
@@ -626,6 +718,8 @@ static int do_crypto(struct cmd_tbl *cmdtp, int flag, int argc, char * const arg
 	test_hash_result();
 
 	rsa_test();
+
+	test_ec_result();
 
 	return 0;
 }
