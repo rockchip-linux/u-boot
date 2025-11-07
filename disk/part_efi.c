7c44903a40db81109b2d73f1ed926ea15234960b
Joseph Chen <chenjh@rock-chips.com>
2025-11-07 11:42:22 +0800

    disk: efi: Sync from next-dev
    Change-Id: I597ef4f33b5f4a748da6af373cd462beadf52877
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- disk/part_efi.c
+++ disk/part_efi.c
@@ -289,7 +289,7 @@ static int __maybe_unused part_get_info_efi(struct blk_desc *desc, int part,
 	if (desc->rawblksz == 4096)
 		b_gpt_nsec = 6;
 
-	/* 
+	/*
 	 * Force allocate 4KB gpt header buffer for emmc and ufs.
 	 */
 	if (!gpt_head)
@@ -300,6 +300,12 @@ static int __maybe_unused part_get_info_efi(struct blk_desc *desc, int part,
 	 * free the pte first if exist and then will malloc and init a new one.
 	 */
 	if (gpt_head && (gpt_head->last_usable_lba + b_gpt_nsec) != desc->rawlba) {
+		if (desc->rawblksz == 4096) {
+			/* realloc gpt header buffer */
+			free(gpt_head);
+			gpt_head = memalign(ARCH_DMA_MINALIGN, desc->rawblksz);
+		}
+
 		if (gpt_pte)
 			free(gpt_pte);
 		gpt_pte = NULL;
