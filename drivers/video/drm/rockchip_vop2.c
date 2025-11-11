0de93da599427dc4d47a75a132bae485281c86f0
Damon Ding <damon.ding@rock-chips.com>
2025-11-11 18:12:01 +0800

    video/drm: vop2: Add support one esmart layer global alpha for rk3576
    To deal with bottom_layer_global_alpha when only have one esmart layer
    at bottom layer. And the cluster global alpha is processed by cluster
    mix.
    
    Change-Id: I2250408abfdd85750f8e75964d881209d3d1a37c
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/video/drm/rockchip_vop2.c
+++ drivers/video/drm/rockchip_vop2.c
@@ -6959,6 +6959,32 @@ static void rk3576_setup_alpha(struct display_state *state)
 			    alpha.dst_alpha_ctrl.val);
 	}
 
+	/* To deal with bottom_layer_global_alpha when only have one esmart layer at
+	 * bottom layer. And the cluster global alpha is processed by cluster mix.
+	 */
+	if (vp->active_layers == 1) {
+		vop2_zpos = &vop2_zpos[0];
+		win_data = vop2_find_win_by_phys_id(vop2, vop2_zpos->plane_id);
+		if (!vop2_cluster_window(win_data) && bottom_layer_global_alpha != 0xff) {
+			alpha_config.src_premulti_en = true;
+			alpha_config.dst_premulti_en = false;
+			alpha_config.src_pixel_alpha_en = true;
+			alpha_config.src_glb_alpha_value = 0xff;
+			alpha_config.dst_glb_alpha_value = bottom_layer_global_alpha;
+			vop2_parse_alpha(&alpha_config, &alpha);
+			offset = (i - 1) * 0x10;
+			vop2_writel(vop2, RK3528_OVL_PORT0_MIX0_SRC_COLOR_CTRL + vp_offset + offset,
+				    alpha.src_color_ctrl.val);
+			vop2_writel(vop2, RK3528_OVL_PORT0_MIX0_DST_COLOR_CTRL + vp_offset + offset,
+				    alpha.dst_color_ctrl.val);
+			vop2_writel(vop2, RK3528_OVL_PORT0_MIX0_SRC_ALPHA_CTRL + vp_offset + offset,
+				    alpha.src_alpha_ctrl.val);
+			vop2_writel(vop2, RK3528_OVL_PORT0_MIX0_DST_ALPHA_CTRL + vp_offset + offset,
+				    alpha.dst_alpha_ctrl.val);
+			i++;
+		}
+	}
+
 	/* Transfer pixel alpha value to next mix */
 	alpha_config.src_premulti_en = true;
 	alpha_config.dst_premulti_en = true;
