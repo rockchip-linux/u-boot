dad34e496327bc1b063c178567b870f7486d7d43
Damon Ding <damon.ding@rock-chips.com>
2025-11-13 15:08:39 +0800

    video/drm: vop2: Sync the default extra layer alpha configs with Kernel for rk3576
    Add two new functions rk3576_extra_layer_sel_for_vp() and
    rk3576_extra_alpha(). The former is to check whether there is extra
    layer for VP0 and update layer select configurations. While the
    latter is to update the extra layer alpha configurations.
    
    They are both for synchronization with the Kernel.
    
    Change-Id: I0fc9bb7d5af9c3bc76f1805a70a23dcdb35755f3
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/video/drm/rockchip_crtc.h
+++ drivers/video/drm/rockchip_crtc.h
@@ -37,6 +37,7 @@ struct rockchip_vp {
 	bool enable;
 	bool xmirror_en;
 	bool sharp_en;
+	bool has_extra_layer;	/* the vp1 layer can merge into vp0 layer after overlay */
 	u8 bg_ovl_dly;
 	u8 primary_plane_id;
 	u8 cursor_plane_id;
