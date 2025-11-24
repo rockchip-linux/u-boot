513a220252d75672766a780d450593922481af7a
Jianwei Zheng <jianwei.zheng@rock-chips.com>
2025-11-24 16:33:06 +0800

    board: rockchip: rk3538: add rockusb support
    Adds rockusb download support for RK3538 SoCs.
    
    Change-Id: I276dc4c2e8c1d5caef03e70825df07d94492ed04
    Signed-off-by: Jianwei Zheng <jianwei.zheng@rock-chips.com>

--- board/rockchip/evb_rk3538/evb_rk3538.c
+++ board/rockchip/evb_rk3538/evb_rk3538.c
@@ -13,4 +13,46 @@
  #include <rockusb.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+#define VO_CRU_BASE		0xFD060000
+#define VO_CRU_SOFTRST_CON01	0x0A04
 
+#ifdef CONFIG_USB_DWC3
+static struct dwc3_device dwc3_device_data = {
+	.maximum_speed = USB_SPEED_HIGH,
+	.base = 0xfc000000,
+	.dr_mode = USB_DR_MODE_PERIPHERAL,
+	.hsphy_mode = USBPHY_INTERFACE_MODE_UTMIW,
+	.index = 0,
+	.dis_u2_susphy_quirk = 1,
+};
+
+int rkusb_dev_bind_to_udc_data(struct udevice *dev)
+{
+	if (IS_ERR_OR_NULL(dev))
+		return -EINVAL;
+
+	dwc3_device_data.dev = dev;
+
+	return 0;
+}
+
+static void usb_reset_otg_controller(void)
+{
+	writel(0x08000800, VO_CRU_BASE + VO_CRU_SOFTRST_CON01);
+	mdelay(1);
+	writel(0x08000000, VO_CRU_BASE + VO_CRU_SOFTRST_CON01);
+	mdelay(1);
+}
+
+int board_usb_init(int index, enum usb_init_type init)
+{
+	usb_reset_otg_controller();
+	return dwc3_uboot_init(&dwc3_device_data);
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
+{
+	dwc3_uboot_exit(index);
+	return 0;
+}
+#endif
