0c688e6c39906a7919fdec999bd0208e92d610a9
Damon Ding <damon.ding@rock-chips.com>
2025-12-04 15:04:40 +0800

    video/drm: vop2: Complete configuration of post-csc convert_mode
    Change-Id: Ifacdf69b097076756c08efca8daea23ba74d567f
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/video/drm/rockchip_vop2.c
+++ drivers/video/drm/rockchip_vop2.c
@@ -2562,6 +2562,10 @@ static void vop3_post_csc_config(struct display_state *state, struct vop2 *vop2)
 	    convert_mode.is_input_full_range != convert_mode.is_output_full_range)
 		post_csc_en = true;
 
+	convert_mode.pixel_depth = 10;
+	convert_mode.coef_precision = 10;
+	convert_mode.swap_channels = 1;
+
 	if (post_csc_en) {
 		rockchip_calc_post_csc(csc, &csc_coef, &convert_mode);
 
