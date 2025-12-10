cc6cba805612dd65d25747acb224eb27ac69850a
Ye Zhang <ye.zhang@rock-chips.com>
2025-12-10 15:30:39 +0800

    rockchip: rk3538: Adjust tsadc bias current
    Change-Id: Ie3cb3a22646c1a1473df55be1d00dd570fda855b
    Signed-off-by: Ye Zhang <ye.zhang@rock-chips.com>

--- arch/arm/mach-rockchip/rk3538/rk3538.c
+++ arch/arm/mach-rockchip/rk3538/rk3538.c
@@ -7,6 +7,7 @@
 #include <fdt_support.h>
 #include <string.h>
 #include <scsi.h>
+#include <spl.h>
 #include <tee/optee.h>
 #include <linux/delay.h>
 #include <asm/armv8/mmu.h>
@@ -18,12 +19,33 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+#define CRU_PHPL_BASE			0xfd080000
+#define CRU_PHPL_GATE_CON01		0x804
+
+#define PHPL_GRF_BASE			0xfd190000
+#define TSADC_CON0			0x1c
+#define TSADC_CON1			0x20
+#define TSADC_CON4			0x2c
+#define TSADC_CON6			0x34
+#define TSADC_ST0			0x110
+#define TSADC_ST1			0x114
+#define TSADC_DEF_WIDTH			0x00010001
+#define TSADC_TARGET_WIDTH		24000
+#define TSADC_EOC_TIMEOUT		20000
+#define TSADC_EOC_MASK			BIT(18)
+#define TSADC_DEF_BIAS			0x7f
+#define TSADC_MIN_BIAS			0x1
+#define TSADC_MAX_BIAS			0x7f
+#define TSADC_UNLOCK_VALUE		0xa5
+#define TSADC_UNLOCK_VALUE_MASK		(0xff << 16)
+#define TSADC_UNLOCK_TRIGGER		BIT(8)
+#define TSADC_UNLOCK_TRIGGER_MASK	(BIT(8) << 16)
+
 #define GPIO1_IOC_BASE		0xFD1D0000
 #define GPIO1A_IOMUX_SEL_1	0x24
 #define GPIO1B_IOMUX_SEL_0	0x28
 #define GPIO1B_IOMUX_SEL_1	0x2c
 #define GPIO1C_IOMUX_SEL_0	0x30
-
 
 #ifdef CONFIG_ARM64
 #include <asm/armv8/mmu.h>
@@ -80,6 +102,48 @@ void rockchip_stimer_init(void)
 	writel(0xffffffff, CONFIG_ROCKCHIP_STIMER_BASE + 0x18);
 	writel(0x00010001, CONFIG_ROCKCHIP_STIMER_BASE + 0x04);
 }
+
+static void tsadc_trigger(void)
+{
+	writel(TSADC_UNLOCK_VALUE | TSADC_UNLOCK_VALUE_MASK,
+	       PHPL_GRF_BASE + TSADC_CON1);
+	writel(TSADC_UNLOCK_TRIGGER | TSADC_UNLOCK_TRIGGER_MASK,
+	       PHPL_GRF_BASE + TSADC_CON1);
+	writel(TSADC_UNLOCK_TRIGGER_MASK, PHPL_GRF_BASE + TSADC_CON1);
+}
+
+static void tsadc_adjust_bias_current(void)
+{
+	u32 i, bias, value = 0, width = 0;
+
+	for (i = 0; i < TSADC_EOC_TIMEOUT; i++) {
+		value = readl(PHPL_GRF_BASE + TSADC_ST0);
+		if ((value & TSADC_EOC_MASK) == TSADC_EOC_MASK)
+			break;
+		udelay(1);
+	}
+
+	if (i == TSADC_EOC_TIMEOUT)
+		printf("Failed to adjust tsadc bias\n");
+
+	value = readl(PHPL_GRF_BASE + TSADC_ST1);
+	width = (value & 0x0000ffff) + ((value & 0xffff0000) >> 16);
+	bias = width * TSADC_DEF_BIAS / TSADC_TARGET_WIDTH;
+	if (bias > TSADC_MAX_BIAS)
+		bias = TSADC_MAX_BIAS;
+	else if (bias < TSADC_MIN_BIAS)
+		bias = TSADC_MIN_BIAS;
+	printf("tsadc width=0x%x %u lo=%u hi=%u, bias=0x%x\n",
+	       value, ((value & 0xffff0000) >> 16) + (value & 0xffff),
+	       value & 0xffff, (value & 0xffff0000) >> 16, bias);
+	writel((TSADC_MAX_BIAS << 16) | bias, PHPL_GRF_BASE + TSADC_CON6);
+	tsadc_trigger();
+}
+
+void spl_rk_board_prepare_for_jump(struct spl_image_info *spl_image)
+{
+	tsadc_adjust_bias_current();
+}
 #endif
 
 #ifndef CONFIG_TPL_BUILD
@@ -92,6 +156,12 @@ int arch_cpu_init(void)
 	writel(0xffff1111, GPIO1_IOC_BASE + GPIO1B_IOMUX_SEL_1); /* FSPI_CSN0/D1/D2/CLK */
 	writel(0x000f0001, GPIO1_IOC_BASE + GPIO1C_IOMUX_SEL_0); /* FSPI_D3 */
 #endif
+
+	/* Enable tsadc phy */
+	writel(0x04000000, CRU_PHPL_BASE+ CRU_PHPL_GATE_CON01);
+	writel(0x007f007f, PHPL_GRF_BASE + TSADC_CON6);
+	writel(0x80088008, PHPL_GRF_BASE + TSADC_CON0);
+	tsadc_trigger();
 #endif
 
 	return 0;
