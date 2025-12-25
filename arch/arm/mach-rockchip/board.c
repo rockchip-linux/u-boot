382cff151a24d119c1491172a151c9b45fa9e714
Joseph Chen <chenjh@rock-chips.com>
2025-12-25 20:20:30 +0800

    rockchip: board: Introduce ramdisk_addr_low_r to override ramdisk_addr_r
    Handle the case:
    The 256MB board uses large ramdisk(i.e. 40MB+) on linux platform, it
    may cause sysmem overlay(no memory) issue when run boot_fit command
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I7d92c6d2a4501af3fbf5a11af27fe6e602977c15

--- arch/arm/mach-rockchip/board.c
+++ arch/arm/mach-rockchip/board.c
@@ -371,6 +371,24 @@ static void env_fixup(void)
 		}
 	}
 #endif
+
+	/*
+	 * ramdisk_addr_low_r to handle the case:
+	 *
+	 * The 256MB board uses large ramdisk(i.e. 40MB+) on linux platform, it
+	 * may cause sysmem overlay(no memory) issue when run boot_fit command:
+	 *
+	 *	0       162MB                             256MB
+	 * 	[ ....... | ramdisk | fit boot.img | U-Boot ]
+	 *
+	 * Do this whether there is BL32 or not.
+	 */
+	if (gd->ram_size > SZ_128M && gd->ram_size <= SZ_256M) {
+		ramdisk_addr = env_get_ulong("ramdisk_addr_low_r", 16, 0);
+		if (ramdisk_addr)
+			env_set_hex("ramdisk_addr_r", ramdisk_addr);
+	}
+
 	/* No BL32 ? */
 	if (!(gd->flags & GD_FLG_BL32_ENABLED)) {
 		/*
