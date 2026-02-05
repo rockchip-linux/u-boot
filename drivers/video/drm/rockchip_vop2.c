96cc34ed217ffbcd0971db4955cbf7abbb599634
Damon Ding <damon.ding@rock-chips.com>
2026-02-05 15:34:11 +0800

    video/drm: vop2: Set mode to RK3576_EDP_OUT_MODE_YUV422 for eDP YUV422 output on RK3572
    The output mode configuration for eDP YUV422 mode is the same as that
    of RK3576.
    
    Change-Id: Ic0d59b85cbefaad5a765d68e88785b71130c137a
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/video/drm/rockchip_vop2.c
+++ drivers/video/drm/rockchip_vop2.c
@@ -6059,7 +6059,7 @@ static int rockchip_vop2_init(struct display_state *state)
 		    conn_state->type == DRM_MODE_CONNECTOR_DisplayPort)
 			conn_state->output_mode = RK3588_DP_OUT_MODE_YUV420;
 	} else if (conn_state->output_mode == ROCKCHIP_OUT_MODE_YUV422) {
-		if (vop2->version == VOP_VERSION_RK3576 &&
+		if (vop2->version >= VOP_VERSION_RK3576 &&
 		    conn_state->type == DRM_MODE_CONNECTOR_eDP)
 			conn_state->output_mode = RK3576_EDP_OUT_MODE_YUV422;
 		else if (vop2->version == VOP_VERSION_RK3588 &&
