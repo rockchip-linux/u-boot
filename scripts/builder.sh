9ee9009fa96dc09f9a73eea227f564f8a9164358
Joseph Chen <chenjh@rock-chips.com>
2026-02-09 21:08:21 +0800

    scripts: builder.sh: Add more platform build rule
    build rkxxx_defconfig and rkxxx.config
    
    Change-Id: I58f6f79ae6c13e3226aacafef57da941054a1ff8
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -14,8 +14,28 @@ platform_list=(
 )
 
 for platform in "${platform_list[@]}"; do
-	echo
-	echo "##################### Building: ${platform} #####################"
-	make distclean
-	./make.sh "${platform}"
+	# All xxx_defconfig
+	for defconfig in configs/${platform}*_defconfig; do
+		if [ -f "$defconfig" ]; then
+			config_name=$(basename "$defconfig" _defconfig)
+			echo
+			echo "##################### Building: ${config_name} #####################"
+			make distclean
+			./make.sh "${config_name}"
+		fi
+	done
+
+	# All xxx.config
+	for configfile in configs/${platform}*.config; do
+		if [ -f "$configfile" ]; then
+			base_defconfig=$(grep -E '^CONFIG_BASE_DEFCONFIG=' "$configfile" | cut -d'=' -f2- | tr -d '"')
+			if [ -n "$base_defconfig" ]; then
+				config_name=$(basename "$configfile" .config)
+				echo
+				echo "##################### Building: ${config_name} (BASE=${base_defconfig}) #####################"
+				make distclean
+				./make.sh "${config_name}"
+			fi
+		fi
+	done
 done
