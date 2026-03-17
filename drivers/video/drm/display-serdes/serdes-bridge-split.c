xiaomeng9597/BDY_G98_RK3588-uboot

video/drm: display-serdes: Analyze and obtain the working mode configured in DTS
Change-Id: I214c370b72697f179ba91224770941b620052c0f
Signed-off-by: Zitong Cai <zitong.cai@rock-chips.com>

drivers/video/drm/display-serdes/serdes-bridge-split.c
@@ -36,8 +36,6 @@ static void serdes_bridge_split_pre_enable(struct rockchip_bridge *bridge)
 	struct udevice *dev = bridge->dev;
 	struct serdes *serdes = dev_get_priv(dev->parent);
 
-	//serdes_bridge_split_split_init(serdes);
-
 	if (serdes->chip_data->bridge_ops->pre_enable)
 		serdes->chip_data->bridge_ops->pre_enable(serdes);
 
@@ -126,20 +124,48 @@ struct rockchip_bridge_funcs serdes_bridge_split_ops = {
 	.detect = serdes_bridge_split_detect,
 };
 
-static int serdes_bridge_split_probe(struct udevice *dev)
+static int serdes_bridge_split_parse_dt(struct udevice *dev)
 {
-	struct rockchip_bridge *bridge;
 	struct serdes *serdes = dev_get_priv(dev->parent);
-	struct mipi_dsi_device *device = dev_get_platdata(dev);
+	struct serdes_bridge_split *serdes_bridge_split = dev_get_priv(dev);
 
 	serdes->sel_mipi = dev_read_bool(dev->parent, "sel-mipi");
 	if (serdes->sel_mipi) {
+		serdes_bridge_split->lanes =
+			dev_read_u32_default(dev, "dsi,lanes", 4);
+		serdes_bridge_split->format =
+			dev_read_u32_default(dev, "dsi,format",
+					     MIPI_DSI_FMT_RGB888);
+		serdes_bridge_split->flags =
+			dev_read_u32_default(dev, "dsi,flags",
+					     MIPI_DSI_MODE_VIDEO |
+					     MIPI_DSI_MODE_VIDEO_SYNC_PULSE);
+		serdes_bridge_split->channel =
+			dev_read_u32_default(dev, "reg", 0);
+	}
+
+	return 0;
+}
+
+static int serdes_bridge_split_probe(struct udevice *dev)
+{
+	int ret;
+	struct rockchip_bridge *bridge;
+	struct serdes *serdes = dev_get_priv(dev->parent);
+	struct mipi_dsi_device *device = dev_get_platdata(dev);
+	struct serdes_bridge_split *serdes_bridge_split = dev_get_priv(dev);
+
+	ret = serdes_bridge_split_parse_dt(dev);
+	if (ret)
+		printf("%s %s parse dt failed, ret=%d\n",
+		       __func__, serdes->dev->name, ret);
+
+	if (serdes->sel_mipi) {
 		device->dev = dev;
-		device->lanes = dev_read_u32_default(dev->parent, "dsi,lanes", 4);
-		device->format = dev_read_u32_default(dev->parent, "dsi,format",
-						      MIPI_DSI_FMT_RGB888);
-		device->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE;
-		device->channel = dev_read_u32_default(dev->parent, "reg", 0);
+		device->lanes = serdes_bridge_split->lanes;
+		device->format = serdes_bridge_split->format;
+		device->mode_flags = serdes_bridge_split->flags;
+		device->channel = serdes_bridge_split->channel;
 	}
 
 	bridge = calloc(1, sizeof(*bridge));
@@ -150,7 +176,8 @@ static int serdes_bridge_split_probe(struct udevice *dev)
 	bridge->dev = dev;
 	bridge->funcs = &serdes_bridge_split_ops;
 
-	serdes->serdes_bridge_split->bridge = bridge;
+	serdes_bridge_split->bridge = bridge;
+	serdes->serdes_bridge_split = serdes_bridge_split;
 
 	SERDES_DBG_MFD("%s: %s %s bridge=%p name=%s device=%p\n",
 		       __func__, serdes->dev->name,

rockchip-linux/u-boot

drivers/video/drm/display-serdes/serdes-bridge-split.c
@@ -36,8 +36,6 @@ static void serdes_bridge_split_pre_enable(struct rockchip_bridge *bridge)
 	struct udevice *dev = bridge->dev;
 	struct serdes *serdes = dev_get_priv(dev->parent);
 
-	//serdes_bridge_split_split_init(serdes);
-
 	if (serdes->chip_data->bridge_ops->pre_enable)
 		serdes->chip_data->bridge_ops->pre_enable(serdes);
 
@@ -126,20 +124,48 @@ struct rockchip_bridge_funcs serdes_bridge_split_ops = {
 	.detect = serdes_bridge_split_detect,
 };
 
-static int serdes_bridge_split_probe(struct udevice *dev)
+static int serdes_bridge_split_parse_dt(struct udevice *dev)
 {
-	struct rockchip_bridge *bridge;
 	struct serdes *serdes = dev_get_priv(dev->parent);
-	struct mipi_dsi_device *device = dev_get_plat(dev);
+	struct serdes_bridge_split *serdes_bridge_split = dev_get_priv(dev);
 
 	serdes->sel_mipi = dev_read_bool(dev->parent, "sel-mipi");
 	if (serdes->sel_mipi) {
+		serdes_bridge_split->lanes =
+			dev_read_u32_default(dev, "dsi,lanes", 4);
+		serdes_bridge_split->format =
+			dev_read_u32_default(dev, "dsi,format",
+					     MIPI_DSI_FMT_RGB888);
+		serdes_bridge_split->flags =
+			dev_read_u32_default(dev, "dsi,flags",
+					     MIPI_DSI_MODE_VIDEO |
+					     MIPI_DSI_MODE_VIDEO_SYNC_PULSE);
+		serdes_bridge_split->channel =
+			dev_read_u32_default(dev, "reg", 0);
+	}
+
+	return 0;
+}
+
+static int serdes_bridge_split_probe(struct udevice *dev)
+{
+	int ret;
+	struct rockchip_bridge *bridge;
+	struct serdes *serdes = dev_get_priv(dev->parent);
+	struct mipi_dsi_device *device = dev_get_plat(dev);
+	struct serdes_bridge_split *serdes_bridge_split = dev_get_priv(dev);
+
+	ret = serdes_bridge_split_parse_dt(dev);
+	if (ret)
+		printf("%s %s parse dt failed, ret=%d\n",
+		       __func__, serdes->dev->name, ret);
+
+	if (serdes->sel_mipi) {
 		device->dev = dev;
-		device->lanes = dev_read_u32_default(dev->parent, "dsi,lanes", 4);
-		device->format = dev_read_u32_default(dev->parent, "dsi,format",
-						      MIPI_DSI_FMT_RGB888);
-		device->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE;
-		device->channel = dev_read_u32_default(dev->parent, "reg", 0);
+		device->lanes = serdes_bridge_split->lanes;
+		device->format = serdes_bridge_split->format;
+		device->mode_flags = serdes_bridge_split->flags;
+		device->channel = serdes_bridge_split->channel;
 	}
 
 	bridge = calloc(1, sizeof(*bridge));
@@ -150,7 +176,8 @@ static int serdes_bridge_split_probe(struct udevice *dev)
 	bridge->dev = dev;
 	bridge->funcs = &serdes_bridge_split_ops;
 
-	serdes->serdes_bridge_split->bridge = bridge;
+	serdes_bridge_split->bridge = bridge;
+	serdes->serdes_bridge_split = serdes_bridge_split;
 
 	SERDES_DBG_MFD("%s: %s %s bridge=%p name=%s device=%p\n",
 		       __func__, serdes->dev->name,