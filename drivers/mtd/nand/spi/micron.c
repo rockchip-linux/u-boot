88fde7b32e28ccc6364a3f145892b32c3265c532
Jon Lin <jon.lin@rock-chips.com>
2026-03-18 15:06:22 +0800

    mtd: spinand: Micron: Support new device MT29F2G01ABBGDWB
    Change-Id: I0420058a72978868eaf05f39f7e2e905c3c7d813
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/micron.c
+++ drivers/mtd/nand/spi/micron.c
@@ -176,6 +176,15 @@ static const struct spinand_info micron_spinand_table[] = {
 					      &update_cache_variants),
 		     0,
 		     SPINAND_ECCINFO(&mt29f4g01abafd_ooblayout, mt29f4g01abafd_ecc_ecc_get_status)),
+	SPINAND_INFO("MT29F2G01ABBGDWB",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0x25),
+		     NAND_MEMORG(1, 2048, 128, 64, 2048, 2, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&mt29f2g01abagd_ooblayout, mt29f2g01abagd_ecc_get_status)),
 };
 
 static const struct spinand_manufacturer_ops micron_spinand_manuf_ops = {
