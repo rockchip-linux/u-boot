8331642210bf2d8846eb0befd97268b57b86c0cd
Jon Lin <jon.lin@rock-chips.com>
2026-04-03 10:35:54 +0800

    mtd: spinand: MK: Fix wrong ecc layout information
    Change-Id: Id3babcfec02d35189f98e482d6b39b6a0829546d
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/mk.c
+++ drivers/mtd/nand/spi/mk.c
@@ -40,7 +40,7 @@ static int mksv1gcl_ooblayout_ecc(struct mtd_info *mtd, int section,
 	if (section > 3)
 		return -ERANGE;
 
-	region->offset = (16 * section) + 8;
+	region->offset = (16 * section) + 6;
 	region->length = 10;
 
 	return 0;
@@ -63,15 +63,33 @@ static const struct mtd_ooblayout_ops mksv1gcl_ooblayout = {
 	.rfree = mksv1gcl_ooblayout_free,
 };
 
+/*
+ *Table 12-5.ECC Error Bits Descriptions
+ *ECC
+ *Ability
+ *ECCS1 ECCS0 ECCSE1 ECCSE0 Description
+ *16
+ *0 0 x x No bit errors were detected during the previous read algorithm
+ *0 1 0 0 1~2 Bit errors were detected and corrected
+ *0 1 0 1 3~4 Bit errors were detected and corrected.
+ *0 1 1 0 5~6 Bit errors were detected and corrected.
+ *0 1 1 1 7~8 Bit errors were detected and corrected.
+ *1 0 0 0 9~10 Bit errors were detected and corrected.
+ *1 0 0 1 11~12 Bit errors were detected and corrected.
+ *1 0 1 0 13~14 Bit errors were detected and corrected.
+ *1 0 1 1 15~16 Bit errors were detected and corrected.
+ *1 1 x x Bit errors greater than ECC capability and not corrected
+ */
 static int mksv1gcl_ecc_get_status(struct spinand_device *spinand,
 				   u8 status)
 {
 	u8 status2;
 	struct spi_mem_op op = SPINAND_GET_FEATURE_OP(MK_REG_STATUS2,
 						      &status2);
+	u8 ecc_status = status & STATUS_ECC_MASK;
 	int ret;
 
-	switch (status & STATUS_ECC_MASK) {
+	switch (ecc_status) {
 	case STATUS_ECC_NO_BITFLIPS:
 		return 0;
 
@@ -87,8 +105,8 @@ static int mksv1gcl_ecc_get_status(struct spinand_device *spinand,
 		ret = spi_mem_exec_op(spinand->slave, &op);
 		if (ret)
 			return ret;
-
-		return ((((status & STATUS_ECC_MASK) >> 2) | (status2 & MK_STATUS2_ECC_MASK)) - 2);
+		return (ecc_status == MK_STATUS_ECC_HAS_BITFLIPS_LESS) ?
+		       (2 + status2 * 2) : (10 + status2 * 2);
 	default:
 		break;
 	}
