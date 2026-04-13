0b1f4f160255c17a160c2ef931703149bbc8f901
Shawn Lin <shawn.lin@rock-chips.com>
2026-04-13 10:05:53 +0800

    rockchip: rk3572: Only enable PCIe NOC timeout
    Only enable PCIe NOC timeout, Other IP likes NVM, DSMC
    and USB have their own timeout design internally. Two
    timeout mechanisms would conflict each other.
    
    Change-Id: Ibbbe8b0adc3fc7211fbd0fe09e49dfbcada91cd1
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>

--- arch/arm/mach-rockchip/rk3572/rk3572.c
+++ arch/arm/mach-rockchip/rk3572/rk3572.c
@@ -330,8 +330,12 @@ int arch_cpu_init(void)
 	 */
 	writel(0xffffffff, SYS_SGRF_BASE + SYS_SGRF_SOC_CON8);
 
-	/* Enable NOC timeout, except DSMC */
-	writel(0xfffffff7, SYS_GRF_BASE + SYS_GRF_SOC_CON10);
+	/*
+	 * Only enable PCIe NOC timeout, Other IP likes NVM, DSMC
+	 * and USB have their own timeout design internally. Two
+	 * timeout mechanisms would conflict each other.
+	 */
+	writel(0x03c003c0, SYS_GRF_BASE + SYS_GRF_SOC_CON10);
 
 	/*
 	 * bit0: Force rdata all 1's when pcie slv err occur
