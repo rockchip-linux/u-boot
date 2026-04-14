892934643566880a663b9ac693a4e6ac4b6cf869
Joseph Chen <chenjh@rock-chips.com>
2026-04-14 18:08:18 +0800

    dm: sysreset: Remove unused reboot()
    Change-Id: I02e82f1bb5bcb334d9d4a40eb70667a125677766
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- drivers/sysreset/sysreset-uclass.c
+++ drivers/sysreset/sysreset-uclass.c
@@ -136,14 +136,6 @@ void reset_cpu(void)
 	sysreset_walk_halt(SYSRESET_WARM);
 }
 
-void reboot(const char *mode)
-{
-	if (mode) {
-		sysreset_walk_prepare(mode);
-	}
-	sysreset_walk_halt(SYSRESET_COLD);
-}
-
 #if IS_ENABLED(CONFIG_SYSRESET_CMD_RESET)
 int do_reset(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
