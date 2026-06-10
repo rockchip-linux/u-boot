61d7c3b1bd298910e8dbc98b229067c7d091d48d
Joseph Chen <chenjh@rock-chips.com>
2026-06-10 14:32:34 +0800

    rockchip: bootargs: Fix PLAIN_VERSION parse error
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: Iac6bced7276daf0d64dd9695857a5edc91a9e21a

--- arch/arm/mach-rockchip/bootargs.c
+++ arch/arm/mach-rockchip/bootargs.c
@@ -180,8 +180,8 @@ static void bootargs_add_fwver(bool verbose)
 	strcat(list2, "-");
 #endif
 	/* optional */
-	if (strcmp(PLAIN_VERSION, "2017.09")) {
-		strncat(list2, p + strlen("2017.09-g"), 10);
+	if (strcmp(PLAIN_VERSION, "v5")) {
+		strncat(list2, p + strlen("v5-g"), 10);
 		strcat(list2, "-");
 	}
 	strcat(list2, U_BOOT_DMI_DATE);
