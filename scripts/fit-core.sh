b7ca82604b13b5b05e1e21a71d32db6cf8f7f3fd
Joseph Chen <chenjh@rock-chips.com>
2026-06-29 15:35:36 +0800

    scripts: fit-core: Don't copy dtc for fit_signcfg_export
    There is a static build dtc in rkbin/tools/.
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: Ibb7dc9de13f042bfb937c26d3be2933af3f0a3e6

--- scripts/fit-core.sh
+++ scripts/fit-core.sh
@@ -20,7 +20,6 @@ SIG_RECOVERY="${FIT_DIR}/recovery.data2sign"
 SIG_CFG_DIR="${FIT_DIR}/fit_signcfg"
 SIG_CONFIG="${SIG_CFG_DIR}/sign.readonly_config"
 MINIALL_INI="${SIG_CFG_DIR}/MINIALL.ini"
-DTC_TOOL="${SIG_CFG_DIR}/dtc"
 # offs
 OFFS_DATA="0x1400"
 # placeholder address
@@ -757,7 +756,6 @@ function fit_signcfg_export()
 		fi
 		cp ${ARG_INI_LOADER} ${MINIALL_INI}
 		cp .config ${SIG_CONFIG}
-		cp ./scripts/dtc/dtc ${DTC_TOOL}
 
 		mkdir -p ${SIG_CFG_DIR}/test_images/
 		cp uboot.img ${SIG_CFG_DIR}/test_images/
