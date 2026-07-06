09f7c8c4a206c0a7fa3fdf1296b80b38129936d4
Jon Lin <jon.lin@rock-chips.com>
2026-07-07 00:08:31 +0800

    mtd: nand: spi: Add ICTHINK XS25D2GASL SPI NAND support
    Add support for ICTHINK XS25D2GASL SPI NAND with the new manufacturer
    driver icthink.c, Kconfig option CONFIG_SPI_NAND_ICTHINK, Makefile
    entry, core registration and header declaration.
    
    Change-Id: If6f2834f04519222e810078b9c4682fd6dbdb899
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -974,6 +974,9 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 #ifdef CONFIG_SPI_NAND_CHUCUN
 	&chucun_spinand_manufacturer,
 #endif
+#ifdef CONFIG_SPI_NAND_ICTHINK
+	&icthink_spinand_manufacturer,
+#endif
 };
 
 static int spinand_manufacturer_match(struct spinand_device *spinand,
--- include/linux/mtd/spinand.h
+++ include/linux/mtd/spinand.h
@@ -277,6 +277,7 @@ extern const struct spinand_manufacturer issi_spinand_manufacturer;
 extern const struct spinand_manufacturer titan_spinand_manufacturer;
 extern const struct spinand_manufacturer mk_spinand_manufacturer;
 extern const struct spinand_manufacturer chucun_spinand_manufacturer;
+extern const struct spinand_manufacturer icthink_spinand_manufacturer;
 
 /**
  * struct spinand_op_variants - SPI NAND operation variants
