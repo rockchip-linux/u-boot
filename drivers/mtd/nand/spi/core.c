5f70cfc9b735b9d2f812f03f4bf945f7a5b75266
Jon Lin <jon.lin@rock-chips.com>
2026-07-07 00:29:38 +0800

    mtd: nand: spi: Add UNILC SCF2BW1I3A SPI NAND support
    Add support for UNILC SCF2BW1I3A SPI NAND with the new manufacturer
    driver unilc.c, Kconfig option CONFIG_SPI_NAND_UNILC, Makefile
    entry, core registration and header declaration.
    
    Change-Id: I9dae49ff0b087dd457acd8987a672c41991dd0a2
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -977,6 +977,9 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 #ifdef CONFIG_SPI_NAND_ICTHINK
 	&icthink_spinand_manufacturer,
 #endif
+#ifdef CONFIG_SPI_NAND_UNILC
+	&unilc_spinand_manufacturer,
+#endif
 };
 
 static int spinand_manufacturer_match(struct spinand_device *spinand,
--- include/linux/mtd/spinand.h
+++ include/linux/mtd/spinand.h
@@ -278,6 +278,7 @@ extern const struct spinand_manufacturer titan_spinand_manufacturer;
 extern const struct spinand_manufacturer mk_spinand_manufacturer;
 extern const struct spinand_manufacturer chucun_spinand_manufacturer;
 extern const struct spinand_manufacturer icthink_spinand_manufacturer;
+extern const struct spinand_manufacturer unilc_spinand_manufacturer;
 
 /**
  * struct spinand_op_variants - SPI NAND operation variants
