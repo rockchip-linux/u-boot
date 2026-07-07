d57a28bd6144083592ea6d1ff80b7634c001a43c
Jon Lin <jon.lin@rock-chips.com>
2026-07-07 23:11:51 +0800

    mtd: spi-nor: xtx: Add support for XT55Q1GF
    Change-Id: Ie28def4ed4f48cdfb70577777f4accd4a6121f1b
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -546,6 +546,8 @@ const struct flash_info spi_nor_ids[] = {
 	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
 	{ INFO("xt25q512f", 0x0b601a, 0, 64 * 1024, 1024,
 	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
+	{ INFO("xt55q1gf", 0x0b601b, 0, 64 * 1024, 2048,
+	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
 #endif
 #ifdef CONFIG_SPI_FLASH_PUYA
 	/* PUYA Semiconductor (Shanghai) Co., Ltd. */
